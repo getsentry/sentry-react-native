9322783bbb337eff19fb15c4efd8899e54ff4713
Daniel Griesser <daniel.griesser.86@gmail.com>
2017-01-05 13:18:51 +0100

    Update bundle command

--- examples/ReactNativeExample/bundle.sh
+++ examples/ReactNativeExample/bundle.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir tmp
 react-native bundle --platform ios --entry-file index.ios.js --dev false --bundle-output ./tmp/main.jsbundle --sourcemap-output ./tmp/sourcemap.js
-sentry-cli releases files "1.0" upload-sourcemaps ./tmp --ext "jsbundle" --ext "js" --url-prefix="/"
-rm -rf ./tmp
+sentry-cli releases files "1.0" upload-sourcemaps ./tmp --url-prefix="/" --rewrite --strip-common-prefix
+#rm -rf ./tmp
