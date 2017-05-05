0c555b51d4f6cbd3914974e1464b4221e181fe34
Daniel Griesser <daniel.griesser.86@gmail.com>
2017-05-05 08:55:24 +0200

    Use ReadableNativeMap/Array, Delete windows folder

--- android/src/main/java/io/sentry/RNSentryModule.java
+++ android/src/main/java/io/sentry/RNSentryModule.java
@@ -8,6 +8,8 @@ import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.ReadableMapKeySetIterator;
+import com.facebook.react.bridge.ReadableNativeArray;
+import com.facebook.react.bridge.ReadableNativeMap;
 import com.facebook.react.bridge.ReadableType;
 import com.facebook.react.bridge.WritableMap;
 
@@ -120,6 +122,7 @@ public class RNSentryModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void captureEvent(ReadableMap event) {
+        ReadableNativeMap castEvent = (ReadableNativeMap)event;
         if (event.hasKey("message")) {
             AndroidEventBuilderHelper helper = new AndroidEventBuilderHelper(this.getReactApplicationContext());
             EventBuilder eventBuilder = new EventBuilder()
@@ -139,20 +142,20 @@ public class RNSentryModule extends ReactContextBaseJavaModule {
             helper.helpBuildingEvent(eventBuilder);
 
             if (this.extra != null) {
-                for (Map.Entry<String, Object> entry : recursivelyDeconstructReadableMap(this.extra).entrySet()) {
+                for (Map.Entry<String, Object> entry : ((ReadableNativeMap)this.extra).toHashMap().entrySet()) {
                     eventBuilder.withExtra(entry.getKey(), entry.getValue());
                 }
             }
-            for (Map.Entry<String, Object> entry : recursivelyDeconstructReadableMap(event.getMap("extra")).entrySet()) {
+            for (Map.Entry<String, Object> entry : castEvent.getMap("extra").toHashMap().entrySet()) {
                 eventBuilder.withExtra(entry.getKey(), entry.getValue());
             }
 
             if (this.tags != null) {
-                for (Map.Entry<String, Object> entry : recursivelyDeconstructReadableMap(this.tags).entrySet()) {
+                for (Map.Entry<String, Object> entry : ((ReadableNativeMap)this.tags).toHashMap().entrySet()) {
                     eventBuilder.withExtra(entry.getKey(), entry.getValue());
                 }
             }
-            for (Map.Entry<String, Object> entry : recursivelyDeconstructReadableMap(event.getMap("tags")).entrySet()) {
+            for (Map.Entry<String, Object> entry : castEvent.getMap("tags").toHashMap().entrySet()) {
                 eventBuilder.withTag(entry.getKey(), entry.getValue().toString());
             }
             Event builtEvent = eventBuilder.build();
@@ -171,7 +174,7 @@ public class RNSentryModule extends ReactContextBaseJavaModule {
     public void activateStacktraceMerging(Promise promise) {
         logger.info("TODO: implement activateStacktraceMerging");
 //        promise.resolve(true);
-        promise.reject("bla", "blub");
+        promise.reject("Sentry", "Stacktrace merging not yet implemented");
     }
 
     private Breadcrumb.Level breadcrumbLevel(String level) {
@@ -216,99 +219,4 @@ public class RNSentryModule extends ReactContextBaseJavaModule {
                 return Level.OFF;
         }
     }
-
-    private void recursivelySetMap(WritableMap params, ReadableMap readableMap) {
-        ReadableMapKeySetIterator iterator = readableMap.keySetIterator();
-        while (iterator.hasNextKey()) {
-            String key = iterator.nextKey();
-            ReadableType type = readableMap.getType(key);
-            switch (type) {
-                case Null:
-                    params.putNull(key);
-                    break;
-                case Boolean:
-                    params.putBoolean(key, readableMap.getBoolean(key));
-                    break;
-                case Number:
-                    params.putDouble(key, readableMap.getDouble(key));
-                    break;
-                case String:
-                    params.putString(key, readableMap.getString(key));
-                    break;
-                case Map:
-                    params.putMap(key, MapUtil.toWritableMap(recursivelyDeconstructReadableMap(readableMap.getMap(key))));
-                    break;
-                case Array:
-                    params.putArray(key, ArrayUtil.toWritableArray(recursivelyDeconstructReadableArray(readableMap.getArray(key)).toArray()));
-                    break;
-                default:
-                    throw new IllegalArgumentException("Could not convert object with key: " + key + ".");
-            }
-
-        }
-    }
-
-    private Map<String, Object> recursivelyDeconstructReadableMap(ReadableMap readableMap) {
-        ReadableMapKeySetIterator iterator = readableMap.keySetIterator();
-        Map<String, Object> deconstructedMap = new HashMap<>();
-        while (iterator.hasNextKey()) {
-            String key = iterator.nextKey();
-            ReadableType type = readableMap.getType(key);
-            switch (type) {
-                case Null:
-                    deconstructedMap.put(key, null);
-                    break;
-                case Boolean:
-                    deconstructedMap.put(key, readableMap.getBoolean(key));
-                    break;
-                case Number:
-                    deconstructedMap.put(key, readableMap.getDouble(key));
-                    break;
-                case String:
-                    deconstructedMap.put(key, readableMap.getString(key));
-                    break;
-                case Map:
-                    deconstructedMap.put(key, recursivelyDeconstructReadableMap(readableMap.getMap(key)));
-                    break;
-                case Array:
-                    deconstructedMap.put(key, recursivelyDeconstructReadableArray(readableMap.getArray(key)));
-                    break;
-                default:
-                    throw new IllegalArgumentException("Could not convert object with key: " + key + ".");
-            }
-
-        }
-        return deconstructedMap;
-    }
-
-    private List<Object> recursivelyDeconstructReadableArray(ReadableArray readableArray) {
-        List<Object> deconstructedList = new ArrayList<>(readableArray.size());
-        for (int i = 0; i < readableArray.size(); i++) {
-            ReadableType indexType = readableArray.getType(i);
-            switch (indexType) {
-                case Null:
-                    deconstructedList.add(i, null);
-                    break;
-                case Boolean:
-                    deconstructedList.add(i, readableArray.getBoolean(i));
-                    break;
-                case Number:
-                    deconstructedList.add(i, readableArray.getDouble(i));
-                    break;
-                case String:
-                    deconstructedList.add(i, readableArray.getString(i));
-                    break;
-                case Map:
-                    deconstructedList.add(i, recursivelyDeconstructReadableMap(readableArray.getMap(i)));
-                    break;
-                case Array:
-                    deconstructedList.add(i, recursivelyDeconstructReadableArray(readableArray.getArray(i)));
-                    break;
-                default:
-                    throw new IllegalArgumentException("Could not convert object at index " + i + ".");
-            }
-        }
-        return deconstructedList;
-    }
-
 }
