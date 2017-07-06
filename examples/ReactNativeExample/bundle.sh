07dc8c4d4bd5297c39b8453d7ba84fd25fa3ba90
Daniel Griesser <daniel.griesser.86@gmail.com>
2017-07-06 13:43:21 +0200

    Remove example project from repo

--- examples/ReactNativeExample/bundle.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mkdir tmp
-react-native bundle --platform ios --entry-file index.ios.js --dev false --bundle-output ./tmp/main.jsbundle --sourcemap-output ./tmp/sourcemap.js
-sentry-cli releases files "1.0" upload-sourcemaps ./tmp --url-prefix="/" --rewrite --strip-common-prefix
-#rm -rf ./tmp
