168213475669ba6146218de1a177b5704b58933f
Daniel Griesser <daniel.griesser.86@gmail.com>
2017-08-01 00:53:02 +0200

    Download syslog and see if error got sent

--- appium/check_run_failues.rb
+++ appium/check_run_failues.rb
@@ -1,4 +1,5 @@
 require 'aws-sdk'
+require 'open-uri'
 
 arn = File.read('./fastlane/.aws.run.arn')
 arn.strip!
@@ -12,10 +13,25 @@ problems = client.list_unique_problems({
 problems.unique_problems.each do |up|
     raise RuntimeError, "No failed tests: #{up.inspect}" unless up.length == 2
     up[1].each do |p|
-        if p.problems[0].test.name == 'test_native_crash' ||
-            p.problems[0].test.name == 'test_throw_error'
+
+        if p.problems[0].test.name == 'test_throw_error'
+            artifacts = client.list_artifacts({
+                type: "FILE",
+                arn: p.problems[0].test.arn
+            })
+            artifacts.artifacts.each do |a|
+                if a.name == 'Syslog'
+                    content = open(a.url).read
+                    exception = content.match(/Sentry - Verbose:: Sending JSON/)
+                    raise RuntimeError, "No JSON SENT: #{p.inspect}" unless !exception.nil?
+                    exception = nil
+                end
+            end
+        end
+        if p.problems[0].test.name == 'test_native_crash'
             exception = p.message.match(/crashed: EXC_/)
             raise RuntimeError, "No crash: #{p.inspect}" unless !exception.nil?
+            exception = nil
         end
     end
 end
