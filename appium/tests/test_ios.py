24d12c4df929f7c9c40829908ddebbb78e5fdeff
Daniel Griesser <daniel.griesser.86@gmail.com>
2017-08-01 17:05:06 +0200

    Fix ios tests

--- appium/tests/test_ios.py
+++ appium/tests/test_ios.py
@@ -18,7 +18,6 @@ def test_send_message(driver):
     assert event['message'] == 'TEST message'
     assert event['extra']['react']
     assert event['tags']['react'] == '1'
-    assert event['sdk'] == False
     assert event['sdk']['integrations'][0] == 'react-native'
     assert len(event['user']) > 0
 
