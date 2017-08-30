59f1bce3273671ff6eaf2514abe2fbc84adb0fdf
Daniel Griesser <daniel.griesser.86@gmail.com>
2017-08-30 09:53:22 +0200

    0.22.0

--- ios/RNSentry.m
+++ ios/RNSentry.m
@@ -9,7 +9,7 @@
 #import <KSCrash/KSCrash.h>
 #import <Sentry/Sentry.h>
 
-NSString *const RNSentryVersionString = @"0.21.2";
+NSString *const RNSentryVersionString = @"0.22.0";
 NSString *const RNSentrySdkName = @"sentry-react-native";
 
 @interface RNSentry()
