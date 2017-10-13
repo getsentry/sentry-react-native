2d10aaa5f9bc7d8bb1dbaa884a0d2fbd3a5f8b9e
Daniel Griesser <daniel.griesser.86@gmail.com>
2017-10-13 13:10:24 +0200

    fix: Use mainthread for RNSentry
    Fixes #259

--- ios/RNSentry.h
+++ ios/RNSentry.h
@@ -3,18 +3,13 @@
 #else
 #import "RCTBridge.h"
 #endif
-#if __has_include(<React/RCTExceptionsManager.h>)
-#import <React/RCTExceptionsManager.h>
-#else
-#import "RCTExceptionsManager.h"
-#endif
 #if __has_include(<React/RCTRootView.h>)
 #import <React/RCTRootView.h>
 #else
 #import "RCTRootView.h"
 #endif
 
-@interface RNSentry : NSObject <RCTBridgeModule, RCTExceptionsManagerDelegate>
+@interface RNSentry : NSObject <RCTBridgeModule>
 
 + (void)installWithRootView:(RCTRootView *)rootView;
 + (void)installWithBridge:(RCTBridge *)bridge;
