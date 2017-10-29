f62c5dc92fc7794c96b925586162b1b8a087dd54
Daniel Griesser <daniel.griesser.86@gmail.com>
2017-10-29 11:35:17 +0100

    fix: travis.yml

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -5,7 +5,6 @@ if [ "$LANE" = "ios" ]; then
     brew install yarn
     brew outdated yarn || brew upgrade yarn
 elif [ "$LANE" = "android" ]; then
-    nvm install 8
     node --version
     curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
     echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
