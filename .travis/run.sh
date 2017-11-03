de9803c20d1830cfe2a5b953f96faee1d9f60074
Jan Michael Auer <jan.auer@sentry.io>
2017-11-03 11:10:10 +0100

    style: Reformat run script

--- .travis/run.sh
+++ .travis/run.sh
@@ -1,27 +1,25 @@
 #!/bin/sh
 set -e
-if [ "$LANE" = "node" ];
-then
-yarn install
-npm run test-typescript
-cd appium
-npm install -g react-native-cli
-make install
-cd example
-npm run test
+if [ "$LANE" = "node" ]; then
+    yarn install
+    npm run test-typescript
+
+    cd appium
+    npm install -g react-native-cli
+    make install
+
+    cd example
+    npm run test
 else
+    cd appium
+    bundle install
+    pip wheel --wheel-dir wheelhouse -r requirements.txt
+    npm install -g react-native-cli
+    react-native -v
 
-cd appium
-bundle install
-pip wheel --wheel-dir wheelhouse -r requirements.txt
-npm install -g react-native-cli
-react-native -v
-
-if [ "$LANE" = "ios" ];
-then
-    make test
-else
-    make test-android
-fi
-
+    if [ "$LANE" = "ios" ]; then
+        make test
+    else
+        make test-android
+    fi
 fi
