659395313009e9192e5635091827547746fdcf6d
Daniel Griesser <daniel.griesser.86@gmail.com>
2017-11-29 15:15:46 +0100

    ci: Update node for ios build

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -3,6 +3,7 @@ set -e
 if [ "$LANE" = "ios" ]; then
     brew update
     brew install yarn
+    brew outdated node || brew upgrade node
     brew outdated yarn || brew upgrade yarn
 elif [ "$LANE" = "android" ]; then
     node --version
