9f5a10883229d52a606ba995d6cff288e25e122e
HazA <daniel.griesser.86@gmail.com>
2018-06-19 09:15:21 +0200

    feat: Remove react application parameter

--- android/src/main/java/io/sentry/RNSentryPackage.java
+++ android/src/main/java/io/sentry/RNSentryPackage.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
@@ -13,20 +12,9 @@ import com.facebook.react.uimanager.ViewManager;
 import com.facebook.react.bridge.JavaScriptModule;
 
 public class RNSentryPackage implements ReactPackage {
-
-    private final ReactApplication reactApplication;
-
-    public RNSentryPackage(ReactApplication reactApplication) {
-        this.reactApplication = reactApplication;
-    }
-
-    public ReactApplication getReactApplication() {
-        return reactApplication;
-    }
-
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-        return Arrays.<NativeModule>asList(new RNSentryModule(reactContext, this.getReactApplication()), new RNSentryEventEmitter(reactContext));
+        return Arrays.<NativeModule>asList(new RNSentryModule(reactContext), new RNSentryEventEmitter(reactContext));
     }
 
     public List<Class<? extends JavaScriptModule>> createJSModules() {
