356f6c8e9a7f1ebc324ba7168b864bec17a701a9
HazA <daniel.griesser.86@gmail.com>
2018-07-26 12:09:20 +0200

    release: 0.38.3

--- ios/RNSentry.m
+++ ios/RNSentry.m
@@ -9,7 +9,7 @@
 #import <KSCrash/KSCrash.h>
 #import <Sentry/Sentry.h>
 
-NSString *const RNSentryVersionString = @"0.38.2";
+NSString *const RNSentryVersionString = @"0.38.3";
 NSString *const RNSentrySdkName = @"sentry-react-native";
 
 @interface RNSentry()
