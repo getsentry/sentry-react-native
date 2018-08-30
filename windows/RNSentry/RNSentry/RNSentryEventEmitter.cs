fccd82802337b1fb217ba909be22f6287b13f4f7
Jon Cox <jon.cox@axsy.com>
2018-08-30 03:20:55 -0700

    initial wrapper for uwp package

---
+++ windows/RNSentry/RNSentry/RNSentryEventEmitter.cs
@@ -0,0 +1,43 @@
+using ReactNative.Bridge;
+using ReactNative.Modules.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RNSentry
+{
+    class RNSentryEventEmitter : ReactContextNativeModuleBase
+    {
+
+        private const string SENTRY_EVENT_SENT_SUCCESSFULLY = "Sentry/eventSentSuccessfully";
+        private const string SENTRY_EVENT_STORED = "Sentry/eventStored";
+
+        public override string Name => "RNSentry";
+
+        public RNSentryEventEmitter(ReactContext reactContext)
+            : base(reactContext)
+        {
+
+        }
+
+        public override IReadOnlyDictionary<string, object> Constants
+        {
+            get
+            {
+                return new Dictionary<string, object>
+                {
+                    { SENTRY_EVENT_SENT_SUCCESSFULLY, SENTRY_EVENT_SENT_SUCCESSFULLY },
+                    { SENTRY_EVENT_STORED, SENTRY_EVENT_STORED },
+                };
+            }
+        }
+
+        public static void sendEvent(ReactContext reactContext, string eventName, object data)
+        {
+            reactContext.GetJavaScriptModule<RCTDeviceEventEmitter>()
+                .emit(eventName, data);
+        }
+    }
+}
--- windows/RNSentry/RNSentry/RNSentryModule.cs
+++ windows/RNSentry/RNSentry/RNSentryModule.cs
@@ -1,14 +1,17 @@
-using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using ReactNative.Bridge;
 using ReactNative.Modules.Core;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using Sentry;
 
 namespace RNSentry
 {
     public class RNSentryModule : ReactContextNativeModuleBase
     {
+        static RavenClient raven;
+        
         public RNSentryModule(ReactContext reactContext)
             : base(reactContext)
         {
@@ -26,10 +29,66 @@ namespace RNSentry
         }
 
         [ReactMethod]
-        public void getMessage(ICallback callback)
+        public void GetMessage(ICallback callback)
         {
             var message = "Hello from Windows";
             callback.Invoke(message);
         }
-    }
+
+        [ReactMethod]
+        public void StartWithDsnString(Sentry.Dsn dsn)
+        {
+            RavenClient.InitializeAsync(dsn, true);
+            raven = RavenClient.Instance;
+        }
+
+        [ReactMethod]
+        public void Crash()
+        {
+            throw new Exception("TEST - RNSentry Windows Native Crash");
+        }
+
+        [ReactMethod]
+        public async void CaptureEvent(JObject evt)
+        {
+            var e = new Exception(evt.ToString());
+            await raven.CaptureExceptionAsync(e, false);
+        }
+
+        [ReactMethod]
+        public async void CaptureBreadcrumb(JObject breadCrumb)
+        {
+            await raven.CaptureMessageAsync(breadCrumb.ToString(), false);
+        }
+
+        [ReactMethod]
+        public void ClearContext()
+        {
+            raven.FlushAsync();
+        }
+
+        [ReactMethod]
+        public void SetLogLevel(string logLevel)
+        {
+           
+        }
+
+        [ReactMethod]
+        public void SetUser(string userId)
+        {
+            raven.SetUser(userId);
+        }
+
+        [ReactMethod]
+        public void SetTags(Dictionary<string, string> tags)
+        {
+            raven.DefaultTags = tags;
+        }
+
+        [ReactMethod]
+        public void AddExtra(string key, string value)
+        {
+            raven.DefaultExtra.Add(key, value);
+        }
+    }   
 }
