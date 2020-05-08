0c7de0fe6655aafcb9996c1b13007ade9ac42806
Daniel Griesser <daniel.griesser.86@gmail.com>
2020-05-08 15:22:01 +0200

    fix: React Native name

--- sample/android/app/src/main/java/com/sample/MainApplication.java
+++ sample/android/app/src/main/java/com/sample/MainApplication.java
@@ -11,6 +11,8 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
+import io.sentry.RNSentryPackage;
+
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
@@ -25,7 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
+           packages.add(new RNSentryPackage());
           return packages;
         }
 
