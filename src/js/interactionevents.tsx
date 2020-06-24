b3433649ffa7c6f7289ba7b95221b5635160fc85
Jenn Mueng <ldrwarlock@gmail.com>
2020-06-24 16:25:32 +0700

    feat: Will log displayName when possible, but will fall back to tree

--- src/js/interactionevents.tsx
+++ src/js/interactionevents.tsx
@@ -1,18 +1,29 @@
 import { addBreadcrumb } from "@sentry/core";
-import { Breadcrumb, Severity } from "@sentry/types";
+import { Severity } from "@sentry/types";
 import * as React from "react";
 import { StyleSheet, View } from "react-native";
 
 // tslint:disable-next-line: interface-over-type-literal
 export type InteractionEventBoundaryProps = {
+  /**
+   * The category assigned to the breadcrumb that is logged by the touch event.
+   */
   breadcrumbCategory?: string;
+  /**
+   * The type assigned to the breadcrumb that is logged by the touch event.
+   */
   breadcrumbType?: string;
-};
-
-// tslint:disable-next-line: interface-over-type-literal
-type InteractionEventBoundaryDefaultProps = {
-  breadcrumbCategory: string;
-  breadcrumbType: string;
+  /**
+   * The max number of components to display when logging a touch's component tree.
+   */
+  maxComponentTreeSize?: number;
+  /**
+   * Component displayName(s) to ignore when logging the touch event. This prevents unhelpful logs such as
+   * "Touch event within element: View" where you still can't tell which View it occurred in.
+   *
+   * By default, only View and Text are ignored. If you pass this prop, don't forget to include them.
+   */
+  ignoredDisplayNames?: string[];
 };
 
 const interactionEventStyles = StyleSheet.create({
@@ -23,8 +34,8 @@ const interactionEventStyles = StyleSheet.create({
 
 const DEFAULT_BREADCRUMB_CATEGORY = "touch";
 const DEFAULT_BREADCRUMB_TYPE = "user";
-
-const DEFAULT_ELEMENT_DISPLAY_NAMES = ["View", "Text"];
+const DEFAULT_MAX_COMPONENT_TREE_SIZE = 20;
+const DEFAULT_IGNORED_DISPLAY_NAMES = ["View", "Text"];
 
 /**
  * Boundary to log breadcrumbs for interaction events.
@@ -32,47 +43,84 @@ const DEFAULT_ELEMENT_DISPLAY_NAMES = ["View", "Text"];
 class InteractionEventBoundary extends React.Component<
   InteractionEventBoundaryProps
 > {
-  public static defaultProps: InteractionEventBoundaryDefaultProps = {
+  public static displayName: string = "InteractionEventBoundary";
+  public static defaultProps: Partial<InteractionEventBoundaryProps> = {
     breadcrumbCategory: DEFAULT_BREADCRUMB_CATEGORY,
     breadcrumbType: DEFAULT_BREADCRUMB_TYPE,
+    ignoredDisplayNames: DEFAULT_IGNORED_DISPLAY_NAMES,
+    maxComponentTreeSize: DEFAULT_MAX_COMPONENT_TREE_SIZE,
   };
 
-  private readonly _logInteraction = (displayName: string): void => {
-    const breadcrumb: Breadcrumb = {
+  private readonly _logInteractionInElement = (displayName: string): void => {
+    addBreadcrumb({
       category: this.props.breadcrumbCategory,
-      type: this.props.breadcrumbType,
       level: Severity.Info,
       message: `Touch event within element: ${displayName}`,
-    };
+      type: this.props.breadcrumbType,
+    });
+  };
 
-    addBreadcrumb(breadcrumb);
+  private readonly _logInteractionInTree = (
+    componentTreeNames: string[]
+  ): void => {
+    addBreadcrumb({
+      category: this.props.breadcrumbCategory,
+      data: { componentTree: componentTreeNames },
+      level: Severity.Info,
+      message: `Touch event within component tree`,
+      type: this.props.breadcrumbType,
+    });
   };
 
   private readonly _onTouchStart = (e: any): void => {
     /* tslint:disable: no-unsafe-any */
     if (e._targetInst) {
       let currentInst = e._targetInst;
-      let name = null;
 
-      /* While there is an instance, and props do not contain the elementIdKey, we keep moving up the instance
-        tree to its parent until we find one with the prop key defined. We also fallback to accessibilityLabel. */
-      while (currentInst) {
-        if (
-          currentInst.elementType &&
-          typeof currentInst.elementType.displayName === "string" &&
-          !DEFAULT_ELEMENT_DISPLAY_NAMES.includes(
-            currentInst.elementType.displayName
-          )
-        ) {
-          name = currentInst.elementType.displayName;
-          break;
+      let displayName = null;
+      const componentTreeNames = [];
+
+      while (
+        currentInst &&
+        // maxComponentTreeSize will always be defined as we have a defaultProps. But ts needs a check so this is here.
+        this.props.maxComponentTreeSize &&
+        componentTreeNames.length < this.props.maxComponentTreeSize
+      ) {
+        if (currentInst.elementType) {
+          if (
+            // If the loop gets to the boundary itself, break.
+            currentInst.elementType.displayName ===
+            InteractionEventBoundary.displayName
+          ) {
+            break;
+          }
+
+          if (
+            typeof currentInst.elementType.displayName === "string" &&
+            // ignore some displayNames for ux
+            this.props.ignoredDisplayNames &&
+            !this.props.ignoredDisplayNames.includes(
+              currentInst.elementType.displayName
+            )
+          ) {
+            /* Break when a displayName is detected, we don't need to log the whole tree now. */
+            displayName = currentInst.elementType.displayName;
+            break;
+          }
+
+          if (typeof currentInst.elementType.name === "string") {
+            /* If this doesn't have a displayName, we log the name and keep going. */
+            componentTreeNames.push(currentInst.elementType.name);
+          }
         }
 
         currentInst = currentInst.return;
       }
 
-      if (name !== null) {
-        this._logInteraction(name);
+      if (displayName !== null) {
+        this._logInteractionInElement(displayName);
+      } else if (componentTreeNames.length > 0) {
+        this._logInteractionInTree(componentTreeNames);
       }
     }
     /* tslint:enable: no-unsafe-any */
