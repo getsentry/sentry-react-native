cf9080f54dabd06b235bda484622342051c0acb6
Jenn Mueng <me@jennmueng.com>
2020-08-12 21:20:31 +0700

    wip: updateGetTestProps

--- sample/utils/getTestProps.ts
+++ sample/utils/getTestProps.ts
@@ -1,7 +1,17 @@
-const getTestProps = (id: string, platform: 'ios' | 'android') => ({
-  accessibilityLabel: platform === 'android' ? id : undefined,
-  accessible: true,
-  testID: platform === 'ios' ? id : undefined,
-});
+/**
+ * Each platform uses different test ids There is a bug in Appium where accessibilityLabel does not work on iOS so we need testID,
+ * and testID does not work on Android so we need accessibilityLabel,
+ * @param id
+ * @param platform
+ */
+const getTestProps = (id: string, platform: 'ios' | 'android') =>
+  platform === 'android'
+    ? {
+        accessibilityLabel: platform === 'android' ? id : undefined,
+        accessible: true,
+      }
+    : {
+        testID: platform === 'ios' ? id : undefined,
+      };
 
 export {getTestProps};
