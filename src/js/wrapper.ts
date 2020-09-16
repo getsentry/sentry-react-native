ca5b260104cc9bdefd302ccdb77719ee7345aaff
cruzach <charliecruzan@gmail.com>
2020-09-16 15:32:39 -0400

    just fix test instead, revert last commit

--- src/js/wrapper.ts
+++ src/js/wrapper.ts
@@ -74,7 +74,7 @@ export const NATIVE = {
       return false;
     }
     if (!options.enableNative) {
-      if (options.enableNativeNagger !== false) {
+      if (options.enableNativeNagger) {
         logger.warn("Note: Native Sentry SDK is disabled.");
       }
       this.enableNative = false;
