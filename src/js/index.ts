e37bad2adbd0e547c0c4b828347b1113183de285
Alex Naiman <alex.naiman.4@gmail.com>
2020-10-26 18:16:11 +0200

    - small fix

--- src/js/index.ts
+++ src/js/index.ts
@@ -44,7 +44,8 @@ export {
 import * as Integrations from "./integrations";
 import { SDK_NAME, SDK_VERSION } from "./version";
 
-export { ReactNativeBackend, ReactNativeOptions } from "./backend";
+export { ReactNativeBackend } from "./backend";
+export { ReactNativeOptions } from './reactNativeOptions';
 export { ReactNativeClient } from "./client";
 // eslint-disable-next-line deprecation/deprecation
 export { init, setDist, setRelease, nativeCrash } from "./sdk";
