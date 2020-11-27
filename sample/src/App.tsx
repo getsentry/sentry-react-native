820a7e409404bf32929b45115b3e8a0dbb991654
jennmueng <ldrwarlock@gmail.com>
2020-11-27 18:18:57 +0700

    ref: Remove unused import

--- sample/src/App.tsx
+++ sample/src/App.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import {View, Text} from 'react-native';
 import {Provider} from 'react-redux';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
