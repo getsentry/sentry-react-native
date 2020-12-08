0054c641bde3a67819c45b75fcb1b835ab40ae09
jennmueng <ldrwarlock@gmail.com>
2020-12-08 20:16:15 +0700

    ref: Comments and formatting for RoutingInstrumentation

--- src/js/tracing/router.ts
+++ src/js/tracing/router.ts
@@ -3,27 +3,36 @@ import { TransactionContext } from "@sentry/types";
 export type RouteListener = (context: TransactionContext) => void;
 
 export interface RoutingInstrumentationType {
+  /**
+   * Registers a listener that's called on every route change with a `TransactionContext`.
+   *
+   * Do not overwrite this unless you know what you are doing.
+   *
+   * @param listener A `RouteListener`
+   */
   registerListener(listener: RouteListener): void;
+  /**
+   * To be called when the route changes, BEFORE the new route mounts.
+   * If this is called after a route mounts the child spans will not be correctly attached.
+   *
+   * @param context A `TransactionContext` used to initialize the transaction.
+   */
   onRouteWillChange(context: TransactionContext): void;
 }
 
 /**
  * Base Routing Instrumentation. Can be used by users to manually instrument custom routers.
+ * Pass this to the tracing integration, and call `onRouteWillChange` every time before a route changes.
  */
 export class RoutingInstrumentation implements RoutingInstrumentationType {
   private _listeners: RouteListener[] = [];
-  /**
-   * Registers a listener to route state.
-   *
-   * Do not overwrite this unless you know what you are doing.
-   */
+
+  /** @inheritdoc */
   public registerListener(listener: RouteListener): void {
     this._listeners.push(listener);
   }
 
-  /**
-   * Calls all route change listeners
-   */
+  /** @inheritdoc */
   public onRouteWillChange(context: TransactionContext): void {
     this._listeners.forEach((listener) => listener(context));
   }
