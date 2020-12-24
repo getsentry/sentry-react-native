e26e27854148821004fff0126f79c8902e98f2d2
jennmueng <ldrwarlock@gmail.com>
2020-12-24 19:44:42 +0700

    feat: Use fetch transport for transactions on android (hack)

--- src/js/transports/native.ts
+++ src/js/transports/native.ts
@@ -1,5 +1,7 @@
+import { Transports } from "@sentry/react";
 import { Event, Response, Transport } from "@sentry/types";
 import { PromiseBuffer, SentryError } from "@sentry/utils";
+import { Platform } from "react-native";
 
 import { NATIVE } from "../wrapper";
 
@@ -8,10 +10,23 @@ export class NativeTransport implements Transport {
   /** A simple buffer holding all requests. */
   protected readonly _buffer: PromiseBuffer<Response> = new PromiseBuffer(30);
 
+  /** Hack that's used to send transactions on Android */
+  private _fetchTransport: Transport;
+
+  constructor(transportOptions: { dsn: string }) {
+    this._fetchTransport = new Transports.FetchTransport(transportOptions);
+  }
+
   /**
    * @inheritDoc
    */
   public sendEvent(event: Event): PromiseLike<Response> {
+    // This is a hack to send transactions through the JS fetch transport on Android
+    // TODO: Remove this hack when android supports transactions through written envelopes.
+    if (Platform.OS === "android" && event.type === "transaction") {
+      return this._fetchTransport.sendEvent(event);
+    }
+
     if (!this._buffer.isReady()) {
       return Promise.reject(
         new SentryError("Not adding Promise due to buffer limit reached.")
