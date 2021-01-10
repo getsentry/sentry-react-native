2c33c86019057d5aa69d7fb17a1cd015811e2fc2
jennmueng <ldrwarlock@gmail.com>
2021-01-10 21:53:58 +0700

    fix: Fix failing native transport test

--- test/transports/native.test.ts
+++ test/transports/native.test.ts
@@ -2,13 +2,17 @@ import { NativeTransport } from "../../src/js/transports/native";
 
 jest.mock("../../src/js/wrapper", () => ({
   NATIVE: {
-    sendEvent: jest.fn(() => Promise.resolve({ status: 200 }))
-  }
+    sendEvent: jest.fn(() => Promise.resolve({ status: 200 })),
+  },
 }));
 
 describe("NativeTransport", () => {
   test("call native sendEvent", async () => {
-    const transport = new NativeTransport();
+    // TODO: Remove this when we remove the fetch transport hack inside the native transport
+    const transport = new NativeTransport({
+      dsn:
+        "https://6890c2f6677340daa4804f8194804ea2@o19635.ingest.sentry.io/148053",
+    });
     await expect(transport.sendEvent({})).resolves.toEqual({ status: 200 });
   });
 });
