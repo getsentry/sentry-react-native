c15b1438341d3f480260a84a09f29268b29745fd
jennmueng <ldrwarlock@gmail.com>
2021-03-18 19:22:16 +0700

    test: onNativeReady callback test

--- test/backend.test.ts
+++ test/backend.test.ts
@@ -1,6 +1,7 @@
 import * as RN from "react-native";
 
 import { ReactNativeBackend } from "../src/js/backend";
+import { NATIVE } from "../src/js/wrapper";
 
 const EXAMPLE_DSN =
   "https://6890c2f6677340daa4804f8194804ea2@o19635.ingest.sentry.io/148053";
@@ -14,7 +15,7 @@ jest.mock(
         nativeClientAvailable: true,
         nativeTransport: true,
         setLogLevel: jest.fn(),
-        startWithDsnString: jest.fn((dsn) => {
+        startWithOptions: jest.fn((dsn) => {
           if (typeof dsn !== "string") {
             throw new Error();
           }
@@ -35,6 +36,11 @@ jest.mock(
   /* virtual allows us to mock modules that aren't in package.json */
   { virtual: true }
 );
+
+afterEach(() => {
+  jest.resetAllMocks();
+  NATIVE.enableNative = true;
+});
 
 describe("Tests ReactNativeBackend", () => {
   describe("initializing the backend", () => {
@@ -84,6 +90,28 @@ describe("Tests ReactNativeBackend", () => {
       await expect(backend.eventFromMessage("test")).resolves.toBeDefined();
       // eslint-disable-next-line deprecation/deprecation
       await expect(RN.YellowBox.ignoreWarnings).toBeCalled();
+    });
+  });
+
+  describe("onNativeReady", () => {
+    test("calls onNativeReady callback with true if Native SDK is initialized", () => {
+      new ReactNativeBackend({
+        dsn: EXAMPLE_DSN,
+        enableNative: true,
+        onNativeReady: ({ nativeDidInitialize }) => {
+          expect(nativeDidInitialize).toBe(true);
+        },
+      });
+    });
+
+    test("calls onNativeReady callback with false if Native SDK was not initialized", () => {
+      new ReactNativeBackend({
+        dsn: EXAMPLE_DSN,
+        enableNative: false,
+        onNativeReady: ({ nativeDidInitialize }) => {
+          expect(nativeDidInitialize).toBe(true);
+        },
+      });
     });
   });
 
