bfede0c2bedf74051ae18eeb39b4e74964f4f587
jennmueng <ldrwarlock@gmail.com>
2021-05-27 13:21:48 +0700

    ref: Cleanup and comments

--- src/js/measurements.ts
+++ src/js/measurements.ts
@@ -4,10 +4,11 @@ import { CustomSamplingContext, TransactionContext } from "@sentry/types";
 import { logger, timestampInSeconds } from "@sentry/utils";
 
 /**
- *
+ * Stall measurement tracker
  */
 export class Stalls {
-  // private _acceptableBusyTime: number = 0;
+  private _acceptableBusyTime: number;
+
   private _totalStallTime: number = 0;
   private _stallCount: number = 0;
   private _longestStall: number = 0;
@@ -15,21 +16,26 @@ export class Stalls {
 
   private _timeout?: number;
 
-  constructor() {
-    // this._acceptableBusyTime = 16.667;
-
-    this._lastInterval = timestampInSeconds() * 1000;
+  constructor(
+    options: {
+      /** How long an event loop can stay "busy" for before being considered a stall. */
+      acceptableBusyTime: number;
+    } = { acceptableBusyTime: 100 }
+  ) {
+    this._acceptableBusyTime = options.acceptableBusyTime;
   }
 
   /**
-   *
+   * Start tracking stalls
    */
   public start(): void {
+    this._lastInterval = timestampInSeconds() * 1000;
+
     this._iteration();
   }
 
   /**
-   *
+   * Stops the stall tracking interval, and returns the measurements
    */
   public finish(): {
     stall_count: { value: number };
@@ -48,26 +54,30 @@ export class Stalls {
   }
 
   /**
-   *
+   * Iteration of the stall tracking interval. Measures how long the timer strayed from its expected time of running, and how
+   * long the stall is for.
    */
   private _iteration(): void {
     const now = timestampInSeconds() * 1000;
     const busyTime = now - this._lastInterval;
 
-    if (busyTime >= 80) {
-      const stallTime = busyTime - 80;
+    if (busyTime >= this._acceptableBusyTime) {
+      const stallTime = busyTime - this._acceptableBusyTime;
       this._stallCount += 1;
       this._totalStallTime += stallTime;
       this._longestStall = Math.max(this._longestStall, stallTime);
     }
 
     this._lastInterval = now;
-    this._timeout = setTimeout(this._iteration.bind(this), 40);
+    this._timeout = setTimeout(
+      this._iteration.bind(this),
+      this._acceptableBusyTime / 5
+    );
   }
 }
 
 /**
- *
+ * Adds React Native's extensions. Needs to be called after @sentry/tracing's extension methods are added
  */
 export function _addTracingExtensions(): void {
   const carrier = getMainCarrier();
@@ -97,13 +107,13 @@ type StartTransactionFunction = (
 ) => Transaction;
 
 /**
- *
+ * Overwrite the startTransaction extension method to start and end stall tracking.
  */
 const _patchStartTransaction = (
   originalStartTransaction: StartTransactionFunction
 ): StartTransactionFunction => {
   /**
-   *
+   * Method to overwrite with
    */
   function _startTransaction(
     this: Hub,
@@ -122,13 +132,19 @@ const _patchStartTransaction = (
     const originalFinish = transaction.finish;
 
     transaction.finish = (endTimestamp: number | undefined) => {
+      // Finish every time unless the stall intervals will keep going
+      const stallMeasurements = stalls.finish();
+
       if (
         typeof endTimestamp === "undefined" &&
         typeof transaction.endTimestamp === "undefined"
       ) {
-        // We can only add stall metrics when the finish time is not manually set.
-        const stallMeasurements = stalls.finish();
-        transaction.setMeasurements(stallMeasurements);
+        if (!transaction.toContext().trimEnd) {
+          // We can only add stall metrics when the finish time is not manually set and the end is not trimmed.
+          transaction.setMeasurements(stallMeasurements);
+        } else {
+          logger.log("Stall metrics not added due to `trimEnd` being true.");
+        }
       } else {
         logger.log("Stall metrics not added due to `endTimestamp` being set.");
       }
