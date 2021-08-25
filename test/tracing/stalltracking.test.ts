4305934cae4c1764f70eeaa6e86e31e9c08e5a37
Jenn Mueng <ldrwarlock@gmail.com>
2021-08-25 18:59:11 -0500

    fix: Revert stall tests

--- test/tracing/stalltracking.test.ts
+++ test/tracing/stalltracking.test.ts
@@ -36,177 +36,177 @@ beforeEach(() => {
 });
 
 describe("StallTracking", () => {
-  // it("Stall tracking detects a JS stall", (done) => {
-  //   const stallTracking = new StallTrackingInstrumentation();
+  it("Stall tracking detects a JS stall", (done) => {
+    const stallTracking = new StallTrackingInstrumentation();
 
-  //   const transaction = new Transaction({
-  //     name: "Test Transaction",
-  //     sampled: true,
-  //   });
-  //   transaction.initSpanRecorder();
+    const transaction = new Transaction({
+      name: "Test Transaction",
+      sampled: true,
+    });
+    transaction.initSpanRecorder();
 
-  //   stallTracking.onTransactionStart(transaction);
+    stallTracking.onTransactionStart(transaction);
 
-  //   expensiveOperation();
+    expensiveOperation();
 
-  //   setTimeout(() => {
-  //     stallTracking.onTransactionFinish(transaction);
-  //     transaction.finish();
+    setTimeout(() => {
+      stallTracking.onTransactionFinish(transaction);
+      transaction.finish();
 
-  //     const measurements = getLastEvent()?.measurements;
+      const measurements = getLastEvent()?.measurements;
 
-  //     expect(measurements).toBeDefined();
-  //     if (measurements) {
-  //       expect(measurements.stall_count.value).toBeGreaterThan(0);
-  //       expect(measurements.stall_longest_time.value).toBeGreaterThan(0);
-  //       expect(measurements.stall_total_time.value).toBeGreaterThan(0);
-  //     }
+      expect(measurements).toBeDefined();
+      if (measurements) {
+        expect(measurements.stall_count.value).toBeGreaterThan(0);
+        expect(measurements.stall_longest_time.value).toBeGreaterThan(0);
+        expect(measurements.stall_total_time.value).toBeGreaterThan(0);
+      }
 
-  //     done();
-  //   }, 500);
-  // });
+      done();
+    }, 500);
+  });
 
-  // it("Stall tracking detects multiple JS stalls", (done) => {
-  //   const stallTracking = new StallTrackingInstrumentation();
+  it("Stall tracking detects multiple JS stalls", (done) => {
+    const stallTracking = new StallTrackingInstrumentation();
 
-  //   const transaction = new Transaction({
-  //     name: "Test Transaction",
-  //     sampled: true,
-  //   });
-  //   transaction.initSpanRecorder();
+    const transaction = new Transaction({
+      name: "Test Transaction",
+      sampled: true,
+    });
+    transaction.initSpanRecorder();
 
-  //   stallTracking.onTransactionStart(transaction);
+    stallTracking.onTransactionStart(transaction);
 
-  //   expensiveOperation();
+    expensiveOperation();
 
-  //   setTimeout(() => {
-  //     expensiveOperation();
-  //   }, 200);
+    setTimeout(() => {
+      expensiveOperation();
+    }, 200);
 
-  //   setTimeout(() => {
-  //     stallTracking.onTransactionFinish(transaction);
-  //     transaction.finish();
-  //     const measurements = getLastEvent()?.measurements;
+    setTimeout(() => {
+      stallTracking.onTransactionFinish(transaction);
+      transaction.finish();
+      const measurements = getLastEvent()?.measurements;
 
-  //     expect(measurements).toBeDefined();
-  //     if (measurements) {
-  //       expect(measurements.stall_count.value).toBe(2);
-  //       expect(measurements.stall_longest_time.value).toBeGreaterThan(0);
-  //       expect(measurements.stall_total_time.value).toBeGreaterThan(0);
-  //     }
+      expect(measurements).toBeDefined();
+      if (measurements) {
+        expect(measurements.stall_count.value).toBe(2);
+        expect(measurements.stall_longest_time.value).toBeGreaterThan(0);
+        expect(measurements.stall_total_time.value).toBeGreaterThan(0);
+      }
 
-  //     done();
-  //   }, 500);
-  // });
+      done();
+    }, 500);
+  });
 
-  // it("Stall tracking timeout is stopped after finishing all transactions (single)", () => {
-  //   const stallTracking = new StallTrackingInstrumentation();
+  it("Stall tracking timeout is stopped after finishing all transactions (single)", () => {
+    const stallTracking = new StallTrackingInstrumentation();
 
-  //   const transaction = new Transaction({
-  //     name: "Test Transaction",
-  //     sampled: true,
-  //   });
+    const transaction = new Transaction({
+      name: "Test Transaction",
+      sampled: true,
+    });
 
-  //   stallTracking.onTransactionStart(transaction);
+    stallTracking.onTransactionStart(transaction);
 
-  //   stallTracking.onTransactionFinish(transaction);
-  //   transaction.finish();
+    stallTracking.onTransactionFinish(transaction);
+    transaction.finish();
 
-  //   const measurements = getLastEvent()?.measurements;
+    const measurements = getLastEvent()?.measurements;
 
-  //   expect(measurements).not.toBe(null);
+    expect(measurements).not.toBe(null);
 
-  //   expect(stallTracking.isTracking).toBe(false);
-  // });
+    expect(stallTracking.isTracking).toBe(false);
+  });
 
-  // it("Stall tracking timeout is stopped after finishing all transactions (multiple)", (done) => {
-  //   const stallTracking = new StallTrackingInstrumentation();
+  it("Stall tracking timeout is stopped after finishing all transactions (multiple)", (done) => {
+    const stallTracking = new StallTrackingInstrumentation();
 
-  //   const transaction0 = new Transaction({
-  //     name: "Test Transaction 0",
-  //     sampled: true,
-  //   });
-  //   const transaction1 = new Transaction({
-  //     name: "Test Transaction 1",
-  //     sampled: true,
-  //   });
-  //   const transaction2 = new Transaction({
-  //     name: "Test Transaction 2",
-  //     sampled: true,
-  //   });
+    const transaction0 = new Transaction({
+      name: "Test Transaction 0",
+      sampled: true,
+    });
+    const transaction1 = new Transaction({
+      name: "Test Transaction 1",
+      sampled: true,
+    });
+    const transaction2 = new Transaction({
+      name: "Test Transaction 2",
+      sampled: true,
+    });
 
-  //   stallTracking.onTransactionStart(transaction0);
-  //   stallTracking.onTransactionStart(transaction1);
+    stallTracking.onTransactionStart(transaction0);
+    stallTracking.onTransactionStart(transaction1);
 
-  //   stallTracking.onTransactionFinish(transaction0);
-  //   transaction0.finish();
-  //   const measurements0 = getLastEvent()?.measurements;
-  //   expect(measurements0).toBeDefined();
+    stallTracking.onTransactionFinish(transaction0);
+    transaction0.finish();
+    const measurements0 = getLastEvent()?.measurements;
+    expect(measurements0).toBeDefined();
 
-  //   setTimeout(() => {
-  //     stallTracking.onTransactionFinish(transaction1);
-  //     transaction1.finish();
-  //     const measurements1 = getLastEvent()?.measurements;
-  //     expect(measurements1).toBeDefined();
-  //   }, 600);
+    setTimeout(() => {
+      stallTracking.onTransactionFinish(transaction1);
+      transaction1.finish();
+      const measurements1 = getLastEvent()?.measurements;
+      expect(measurements1).toBeDefined();
+    }, 600);
 
-  //   setTimeout(() => {
-  //     stallTracking.onTransactionStart(transaction2);
+    setTimeout(() => {
+      stallTracking.onTransactionStart(transaction2);
 
-  //     setTimeout(() => {
-  //       stallTracking.onTransactionFinish(transaction2);
-  //       transaction2.finish();
-  //       const measurements2 = getLastEvent()?.measurements;
-  //       expect(measurements2).not.toBe(null);
+      setTimeout(() => {
+        stallTracking.onTransactionFinish(transaction2);
+        transaction2.finish();
+        const measurements2 = getLastEvent()?.measurements;
+        expect(measurements2).not.toBe(null);
 
-  //       expect(stallTracking.isTracking).toBe(false);
+        expect(stallTracking.isTracking).toBe(false);
 
-  //       done();
-  //     }, 200);
-  //   }, 500);
+        done();
+      }, 200);
+    }, 500);
 
-  //   // If the stall tracking does not correctly stop, the process will keep running. We detect this by passing --detectOpenHandles to jest.
-  // });
+    // If the stall tracking does not correctly stop, the process will keep running. We detect this by passing --detectOpenHandles to jest.
+  });
 
-  // it("Stall tracking returns measurements format on finish", () => {
-  //   const stallTracking = new StallTrackingInstrumentation();
+  it("Stall tracking returns measurements format on finish", () => {
+    const stallTracking = new StallTrackingInstrumentation();
 
-  //   const transaction = new Transaction({
-  //     name: "Test Transaction",
-  //     sampled: true,
-  //   });
+    const transaction = new Transaction({
+      name: "Test Transaction",
+      sampled: true,
+    });
 
-  //   stallTracking.onTransactionStart(transaction);
+    stallTracking.onTransactionStart(transaction);
 
-  //   stallTracking.onTransactionFinish(transaction);
-  //   transaction.finish();
-  //   const measurements = getLastEvent()?.measurements;
+    stallTracking.onTransactionFinish(transaction);
+    transaction.finish();
+    const measurements = getLastEvent()?.measurements;
 
-  //   expect(measurements).toBeDefined();
+    expect(measurements).toBeDefined();
 
-  //   if (measurements) {
-  //     expect(measurements.stall_count.value).toBe(0);
-  //     expect(measurements.stall_longest_time.value).toBe(0);
-  //     expect(measurements.stall_total_time.value).toBe(0);
-  //   }
-  // });
+    if (measurements) {
+      expect(measurements.stall_count.value).toBe(0);
+      expect(measurements.stall_longest_time.value).toBe(0);
+      expect(measurements.stall_total_time.value).toBe(0);
+    }
+  });
 
-  // it("Stall tracking returns null on a custom endTimestamp that is not a span's", () => {
-  //   const stallTracking = new StallTrackingInstrumentation();
+  it("Stall tracking returns null on a custom endTimestamp that is not a span's", () => {
+    const stallTracking = new StallTrackingInstrumentation();
 
-  //   const transaction = new Transaction({
-  //     name: "Test Transaction",
-  //     sampled: true,
-  //   });
+    const transaction = new Transaction({
+      name: "Test Transaction",
+      sampled: true,
+    });
 
-  //   stallTracking.onTransactionStart(transaction);
+    stallTracking.onTransactionStart(transaction);
 
-  //   stallTracking.onTransactionFinish(transaction, Date.now() / 1000);
-  //   transaction.finish();
-  //   const measurements = getLastEvent()?.measurements;
+    stallTracking.onTransactionFinish(transaction, Date.now() / 1000);
+    transaction.finish();
+    const measurements = getLastEvent()?.measurements;
 
-  //   expect(measurements).toBeUndefined();
-  // });
+    expect(measurements).toBeUndefined();
+  });
 
   it("Stall tracking supports endTimestamp that is from the last span (trimEnd case)", (done) => {
     const stallTracking = new StallTrackingInstrumentation();
@@ -368,125 +368,125 @@ describe("StallTracking", () => {
     }, 100);
   });
 
-  // it("Stall tracking ignores unfinished spans in normal transactions", (done) => {
-  //   const stallTracking = new StallTrackingInstrumentation();
+  it("Stall tracking ignores unfinished spans in normal transactions", (done) => {
+    const stallTracking = new StallTrackingInstrumentation();
 
-  //   const transaction = new Transaction({
-  //     name: "Test Transaction",
-  //     trimEnd: true,
-  //     sampled: true,
-  //   });
-  //   transaction.initSpanRecorder();
+    const transaction = new Transaction({
+      name: "Test Transaction",
+      trimEnd: true,
+      sampled: true,
+    });
+    transaction.initSpanRecorder();
 
-  //   stallTracking.onTransactionStart(transaction);
+    stallTracking.onTransactionStart(transaction);
 
-  //   // Span is never finished.
-  //   transaction.startChild({
-  //     description: "Test Span",
-  //   });
+    // Span is never finished.
+    transaction.startChild({
+      description: "Test Span",
+    });
 
-  //   // Span will be finished
-  //   const span = transaction.startChild({
-  //     description: "To Finish",
-  //   });
+    // Span will be finished
+    const span = transaction.startChild({
+      description: "To Finish",
+    });
 
-  //   setTimeout(() => {
-  //     span.finish();
-  //   }, 100);
+    setTimeout(() => {
+      span.finish();
+    }, 100);
 
-  //   setTimeout(() => {
-  //     stallTracking.onTransactionFinish(transaction);
-  //     transaction.finish();
-  //     const measurements = getLastEvent()?.measurements;
+    setTimeout(() => {
+      stallTracking.onTransactionFinish(transaction);
+      transaction.finish();
+      const measurements = getLastEvent()?.measurements;
 
-  //     expect(measurements).toBeDefined();
+      expect(measurements).toBeDefined();
 
-  //     if (measurements) {
-  //       expect(measurements.stall_count.value).toEqual(expect.any(Number));
-  //       expect(measurements.stall_longest_time.value).toEqual(
-  //         expect.any(Number)
-  //       );
-  //       expect(measurements.stall_total_time.value).toEqual(expect.any(Number));
-  //     }
+      if (measurements) {
+        expect(measurements.stall_count.value).toEqual(expect.any(Number));
+        expect(measurements.stall_longest_time.value).toEqual(
+          expect.any(Number)
+        );
+        expect(measurements.stall_total_time.value).toEqual(expect.any(Number));
+      }
 
-  //     done();
-  //   }, 500);
-  // });
+      done();
+    }, 500);
+  });
 
-  // it("Stall tracking only measures stalls inside the final time when trimEnd is used", (done) => {
-  //   const stallTracking = new StallTrackingInstrumentation();
+  it("Stall tracking only measures stalls inside the final time when trimEnd is used", (done) => {
+    const stallTracking = new StallTrackingInstrumentation();
 
-  //   const transaction = new Transaction({
-  //     name: "Test Transaction",
-  //     trimEnd: true,
-  //     sampled: true,
-  //   });
-  //   transaction.initSpanRecorder();
+    const transaction = new Transaction({
+      name: "Test Transaction",
+      trimEnd: true,
+      sampled: true,
+    });
+    transaction.initSpanRecorder();
 
-  //   stallTracking.onTransactionStart(transaction);
+    stallTracking.onTransactionStart(transaction);
 
-  //   // Span will be finished
-  //   const span = transaction.startChild({
-  //     description: "To Finish",
-  //   });
+    // Span will be finished
+    const span = transaction.startChild({
+      description: "To Finish",
+    });
 
-  //   setTimeout(() => {
-  //     span.finish();
-  //   }, 200);
+    setTimeout(() => {
+      span.finish();
+    }, 200);
 
-  //   setTimeout(() => {
-  //     stallTracking.onTransactionFinish(transaction);
-  //     transaction.finish();
-  //     const measurements = getLastEvent()?.measurements;
+    setTimeout(() => {
+      stallTracking.onTransactionFinish(transaction);
+      transaction.finish();
+      const measurements = getLastEvent()?.measurements;
 
-  //     expect(measurements).toBeDefined();
+      expect(measurements).toBeDefined();
 
-  //     if (measurements) {
-  //       expect(measurements.stall_count.value).toEqual(1);
-  //       expect(measurements.stall_longest_time.value).toEqual(
-  //         expect.any(Number)
-  //       );
-  //       expect(measurements.stall_total_time.value).toEqual(expect.any(Number));
-  //     }
+      if (measurements) {
+        expect(measurements.stall_count.value).toEqual(1);
+        expect(measurements.stall_longest_time.value).toEqual(
+          expect.any(Number)
+        );
+        expect(measurements.stall_total_time.value).toEqual(expect.any(Number));
+      }
 
-  //     done();
-  //   }, 500);
+      done();
+    }, 500);
 
-  //   setTimeout(() => {
-  //     // this should be run after the span finishes, and not logged.
-  //     expensiveOperation();
-  //   }, 300);
+    setTimeout(() => {
+      // this should be run after the span finishes, and not logged.
+      expensiveOperation();
+    }, 300);
 
-  //   expensiveOperation();
-  // });
+    expensiveOperation();
+  });
 
-  // it("Stall tracking does not track the first transaction if more than 10 are running", () => {
-  //   const stallTracking = new StallTrackingInstrumentation();
+  it("Stall tracking does not track the first transaction if more than 10 are running", () => {
+    const stallTracking = new StallTrackingInstrumentation();
 
-  //   const transactions = new Array(11).fill(0).map((_, i) => {
-  //     const transaction = new Transaction({
-  //       name: `Test Transaction ${i}`,
-  //       sampled: true,
-  //     });
+    const transactions = new Array(11).fill(0).map((_, i) => {
+      const transaction = new Transaction({
+        name: `Test Transaction ${i}`,
+        sampled: true,
+      });
 
-  //     stallTracking.onTransactionStart(transaction);
+      stallTracking.onTransactionStart(transaction);
 
-  //     return transaction;
-  //   });
+      return transaction;
+    });
 
-  //   stallTracking.onTransactionFinish(transactions[0]);
-  //   transactions[0].finish();
-  //   const measurements0 = getLastEvent()?.measurements;
-  //   expect(measurements0).toBeUndefined();
+    stallTracking.onTransactionFinish(transactions[0]);
+    transactions[0].finish();
+    const measurements0 = getLastEvent()?.measurements;
+    expect(measurements0).toBeUndefined();
 
-  //   stallTracking.onTransactionFinish(transactions[1]);
-  //   transactions[1].finish();
-  //   const measurements1 = getLastEvent()?.measurements;
-  //   expect(measurements1).toBeDefined();
+    stallTracking.onTransactionFinish(transactions[1]);
+    transactions[1].finish();
+    const measurements1 = getLastEvent()?.measurements;
+    expect(measurements1).toBeDefined();
 
-  //   transactions.slice(2).forEach((transaction) => {
-  //     stallTracking.onTransactionFinish(transaction);
-  //     transaction.finish();
-  //   });
-  // });
+    transactions.slice(2).forEach((transaction) => {
+      stallTracking.onTransactionFinish(transaction);
+      transaction.finish();
+    });
+  });
 });
