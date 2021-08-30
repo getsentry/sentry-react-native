33e5f408c710cf5ec746f8d716cb76e92cda9c85
Jenn Mueng <ldrwarlock@gmail.com>
2021-08-30 16:44:24 -0500

    fix: handle undefined event.sdk edge case

--- src/js/integrations/sdkinfo.ts
+++ src/js/integrations/sdkinfo.ts
@@ -38,7 +38,7 @@ export class SdkInfo implements Integration {
 
       event.platform = event.platform || "javascript";
       event.sdk = {
-        ...event.sdk,
+        ...(event.sdk ?? {}),
         name: SDK_NAME,
         packages: [
           ...((event.sdk && event.sdk.packages) || []),
