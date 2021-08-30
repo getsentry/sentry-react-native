3d3138a1d2ddf6e18d407393835c25a9678142cd
Jenn Mueng <ldrwarlock@gmail.com>
2021-08-30 16:31:32 -0500

    test: Add eventorigin integration test

---
+++ test/integrations/eventorigin.test.ts
@@ -0,0 +1,26 @@
+import { Event } from "@sentry/types";
+
+import { EventOrigin } from "../../src/js/integrations";
+
+describe("Event Origin", () => {
+  it("Adds event.origin and event.environment javascript tags to events", (done) => {
+    const integration = new EventOrigin();
+
+    const mockEvent: Event = {};
+
+    integration.setupOnce(async (eventProcessor) => {
+      const processedEvent = await eventProcessor(mockEvent);
+
+      expect(processedEvent).toBeDefined();
+      if (processedEvent) {
+        expect(processedEvent.tags).toBeDefined();
+        if (processedEvent.tags) {
+          expect(processedEvent.tags["event.origin"]).toBe("javascript");
+          expect(processedEvent.tags["event.environment"]).toBe("javascript");
+        }
+      }
+
+      done();
+    });
+  });
+});
