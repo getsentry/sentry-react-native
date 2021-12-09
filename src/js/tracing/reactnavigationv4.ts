3c3cc355ff869d1850883694cfc9cf7dfca23438
Manoel Aranda Neto <5731772+marandaneto@users.noreply.github.com>
2021-12-09 13:02:13 +0100

    fix: ReactNavigationV4Instrumentation null when evaluating 'state.routes' (#1940)

--- src/js/tracing/reactnavigationv4.ts
+++ src/js/tracing/reactnavigationv4.ts
@@ -200,9 +200,19 @@ class ReactNavigationV4Instrumentation extends InternalRoutingInstrumentation {
    * To be called on navigation state changes and creates the transaction.
    */
   private _onStateChange(
-    state: NavigationStateV4,
+    state: NavigationStateV4 | undefined,
     updateLatestTransaction: boolean = false
   ): void {
+    // it's not guaranteed that a state is always produced.
+    // see: https://github.com/react-navigation/react-navigation/blob/45d419be93c34e900e8734ce98321ae875ac4997/packages/core/src/routers/SwitchRouter.js?rgh-link-date=2021-09-25T12%3A43%3A36Z#L301
+    if (!state || state === undefined) {
+      logger.warn(
+        "[ReactNavigationV4Instrumentation] onStateChange called without a valid state."
+      );
+
+      return;
+    }
+
     const currentRoute = this._getCurrentRouteFromState(state);
 
     // If the route is a different key, this is so we ignore actions that pertain to the same screen.
