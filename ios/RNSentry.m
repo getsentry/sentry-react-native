55fbf6310b25af2b6f1ac300808c62a5f9461a96
Manoel Aranda Neto <marandaneto@gmail.com>
2022-06-29 09:53:16 +0200

    remove comment

--- ios/RNSentry.m
+++ ios/RNSentry.m
@@ -274,7 +274,6 @@ RCT_EXPORT_METHOD(captureEnvelope:(NSDictionary * _Nonnull)envelopeDict
     if ([NSJSONSerialization isValidJSONObject:envelopeDict]) {
         SentrySdkInfo *sdkInfo = [[SentrySdkInfo alloc] initWithDict:envelopeDict[@"header"]];
         SentryId *eventId = [[SentryId alloc] initWithUUIDString:envelopeDict[@"header"][@"event_id"]];
-        // forward the baggage header, right now only trace_id and public_key
         SentryTraceContext *traceContext = [[SentryTraceContext alloc] initWithDict:envelopeDict[@"header"][@"trace"]];
         SentryEnvelopeHeader *envelopeHeader = [[SentryEnvelopeHeader alloc] initWithId:eventId sdkInfo:sdkInfo traceContext:traceContext];
 
