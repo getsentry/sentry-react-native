7111890703928e6f97a0558821b98a3b8418bcce
Ivan Dlugos <6349682+vaind@users.noreply.github.com>
2022-07-06 14:07:33 +0200

    fixup: wizard dependency updater (#2348)

--- scripts/update-javascript.sh
+++ scripts/update-javascript.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+tagPrefix=''
 repo="https://github.com/getsentry/sentry-javascript.git"
 packages=('@sentry/browser' '@sentry/core' '@sentry/hub' '@sentry/integrations' '@sentry/react' '@sentry/tracing' '@sentry/types' '@sentry/utils')
 packages+=('@sentry-internal/eslint-config-sdk' '@sentry-internal/eslint-plugin-sdk')
