d608085cada9e10d7949a56c4267695830412c45
Ivan Dlugos <dlugos.ivan@gmail.com>
2022-07-07 15:39:22 +0200

    chore: sample RN - match non-preview versions only

--- sample/scripts/update-rn.sh
+++ sample/scripts/update-rn.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-tagPrefix='v' # wizard has a prefix in the repo, but the package.json doesn't have that - we must align
+tagPrefix='v' # react-native has a prefix in the repo, but the package.json doesn't have that - we must align
 repo="https://github.com/facebook/react-native.git"
 packages=('react-native')
 
