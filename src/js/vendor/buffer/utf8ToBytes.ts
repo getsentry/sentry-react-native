d98a589e90a869c75d48a00a5f69587d947256d9
Kryštof Woldřich <krystof.woldrich@sentry.io>
2022-09-13 15:11:21 +0200

    Fix lint, add vendor buffer desc

--- src/js/vendor/buffer/utf8ToBytes.ts
+++ src/js/vendor/buffer/utf8ToBytes.ts
@@ -1,6 +1,12 @@
+/* eslint-disable */
+
 /**
  * Convert a string to a byte array
  *
+ * This is a utf8ToBytes function from the buffer module (with added types)
+ * https://github.com/feross/buffer/blob/795bbb5bda1b39f1370ebd784bea6107b087e3a7/index.js#L1956
+ *
+ * License: MIT (https://github.com/feross/buffer)
  */
 export function utf8ToBytes(string: string, units?: number): number[] {
   units = units || Infinity
