14bf55666c717a1c0ab9f3c7f823cdfff616d69d
Kryštof Woldřich <krystof.woldrich@sentry.io>
2022-09-20 13:15:33 +0200

    Remove launch db update, fix yarn lock

--- scripts/update-launch-database.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-lsregister=/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister
-simulatorApp=/Applications/Xcode.app/Contents/Developer/Applications/Simulator.app
-
-$lsregister -f $simulatorApp
