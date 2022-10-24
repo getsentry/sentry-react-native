4a86d2eb230bed0e3fdfc7a1f59946aa3327409c
Krystof Woldrich <krystofwoldrich@gmail.com>
2022-10-24 10:35:25 +0200

    Add JS docs

--- src/js/utils/worldwide.ts
+++ src/js/utils/worldwide.ts
@@ -1,5 +1,6 @@
 import { GLOBAL_OBJ, InternalGlobal } from '@sentry/utils';
 
+/** Internal Global object interface with common and Sentry specific properties */
 export interface ReactNativeInternalGlobal extends InternalGlobal {
   __sentry_rn_v4_registered?: boolean;
   __sentry_rn_v5_registered?: boolean;
@@ -7,4 +8,5 @@ export interface ReactNativeInternalGlobal extends InternalGlobal {
   Promise: unknown;
 }
 
+/** Get's the global object for the current JavaScript runtime */
 export const RN_GLOBAL_OBJ = GLOBAL_OBJ as ReactNativeInternalGlobal;
