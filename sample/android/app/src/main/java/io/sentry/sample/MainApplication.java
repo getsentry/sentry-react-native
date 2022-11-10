6c45ab811756f63def82c2c96b801e7641244385
Krystof Woldrich <krystofwoldrich@gmail.com>
2022-11-10 17:18:33 +0100

    Fix RN 0.70.x ios and android builds

--- sample/android/app/src/main/java/io/sentry/sample/MainApplication.java
+++ sample/android/app/src/main/java/io/sentry/sample/MainApplication.java
@@ -7,6 +7,7 @@ import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
+import com.facebook.react.config.ReactFeatureFlags;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
