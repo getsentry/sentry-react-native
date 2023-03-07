3d699fc1cf089d618e3827551e4793e720fb7e93
Kryštof Woldřich <31292499+krystofwoldrich@users.noreply.github.com>
2023-03-07 15:32:50 +0100

    chore(sample): Update new arch sample (#2853)
    - add sample module (legacy)
    - make it compatible with legacy arch
    - clean up `before_send` console log
    - refactor sentry init options

--- sample-new-architecture/ios/sampleNewArchitecture/AppDelegate.mm
+++ sample-new-architecture/ios/sampleNewArchitecture/AppDelegate.mm
@@ -3,7 +3,10 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/CoreModulesPlugins.h>
 #import <ReactCommon/RCTTurboModuleManager.h>
+
+#ifdef RCT_NEW_ARCH_ENABLED
 #import <NativeSampleModule.h>
+#endif
 
 @interface AppDelegate () <RCTTurboModuleManagerDelegate> {}
 @end
@@ -44,9 +47,11 @@
 - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:(const std::string &)name
                                                       jsInvoker:(std::shared_ptr<facebook::react::CallInvoker>)jsInvoker
 {
+#ifdef RCT_NEW_ARCH_ENABLED
   if (name == "NativeSampleModule") {
     return std::make_shared<facebook::react::NativeSampleModule>(jsInvoker);
   }
+#endif
   return nullptr;
 }
 
