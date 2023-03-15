915bc1a87063c896fd4987b1f7a69d984a56e420
Krystof Woldrich <krystofwoldrich@gmail.com>
2023-03-15 14:17:05 +0100

    Update before finish callback types

--- src/js/tracing/transaction.ts
+++ src/js/tracing/transaction.ts
@@ -1,5 +1,5 @@
 import type { IdleTransaction } from '@sentry/tracing';
-import type { BeforeFinishCallback } from '@sentry/tracing/types/idletransaction';
+import type { BeforeFinishCallback } from '@sentry/core/types/tracing/idletransaction';
 import { logger } from '@sentry/utils';
 
 /**
