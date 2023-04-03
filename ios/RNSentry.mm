187180ab8bf5d42241d7f0de7e0321d5fadd477b
Krystof Woldrich <krystofwoldrich@gmail.com>
2023-04-03 16:22:47 +0200

    feature(ios): Add processor count to device info

--- ios/RNSentry.mm
+++ ios/RNSentry.mm
@@ -203,6 +203,19 @@ RCT_EXPORT_METHOD(fetchNativeDeviceContexts:(RCTPromiseResolveBlock)resolve
         }
     }];
 
+    NSDictionary<NSString *, id> *extraContext = [PrivateSentrySDKOnly getExtraContext];
+
+    NSMutableDictionary<NSString *, NSDictionary<NSString *, id> *> *deviceContext = [contexts[@"context"][@"device"] mutableCopy];
+    [deviceContext addEntriesFromDictionary:extraContext[@"device"]];
+
+    NSMutableDictionary<NSString *, NSDictionary<NSString *, id> *> *appContext = [contexts[@"context"][@"app"] mutableCopy];
+    [appContext addEntriesFromDictionary:extraContext[@"app"]];
+
+    NSMutableDictionary<NSString *, NSDictionary<NSString *, id> *> *context = [contexts[@"context"] mutableCopy];
+    [context setValue:deviceContext forKey:@"device"];
+    [context setValue:appContext forKey:@"app"];
+    [contexts setValue:context forKey:@"context"];
+
     resolve(contexts);
 }
 
