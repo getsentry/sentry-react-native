f60aecd13546a6841df6d28dda2e4c6be287113c
Krystof Woldrich <krystofwoldrich@gmail.com>
2023-04-04 16:54:34 +0200

    WIP! Upgrade to 0.72 sdk, android sample

--- sample-new-architecture/src/Screens/HomeScreen.tsx
+++ sample-new-architecture/src/Screens/HomeScreen.tsx
@@ -157,6 +157,16 @@ const HomeScreen = (props: Props) => {
           }}
         />
         <Button
+          title="Capture HTTP Client Error"
+          onPress={async () => {
+            try {
+              fetch('http://localhost:8081/not-found');
+            } catch (error) {
+              //ignore the error, it will be send to Sentry
+            }
+          }}
+        />
+        <Button
           title="Auto Tracing Example"
           onPress={() => {
             props.navigation.navigate('Tracker');
