ca1cf473b6fbf8a79b5f5d234177089d3c349c03
Krystof Woldrich <krystof.woldrich@sentry.io>
2023-06-22 17:13:28 +0200

    Fix sample lint

--- sample-new-architecture/__tests__/App-test.tsx
+++ sample-new-architecture/__tests__/App-test.tsx
@@ -5,7 +5,7 @@
 import React from 'react';
 
 // Note: import explicitly to use the types shipped with jest.
-import {it} from '@jest/globals';
+import { it } from '@jest/globals';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
