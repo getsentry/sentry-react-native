a80b3c25b9de29daf413f30bb2a2b08c605f68d6
Krystof Woldrich <krystof.woldrich@sentry.io>
2023-06-29 13:33:42 +0200

    POC: Add preModule for debugIds

--- sample-new-architecture/metro.config.js
+++ sample-new-architecture/metro.config.js
@@ -1,8 +1,30 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 const path = require('path');
 const blacklist = require('metro-config/src/defaults/exclusionList');
+const bundleToString = require('metro/src/lib/bundleToString');
+const baseJSBundle = require('metro/src/DeltaBundler/Serializers/baseJSBundle');
+const CountingSet = require('metro/src/lib/CountingSet').default;
+const countLines = require('metro/src/lib/countLines');
 
 const parentDir = path.resolve(__dirname, '..');
+
+const debugIdCode = 'console.log("debugId");';
+const debugIdModule = {
+  dependencies: new Map(),
+  getSource: () => Buffer.from(debugIdCode),
+  inverseDependencies: new CountingSet(),
+  path: '__debugid__',
+  output: [
+    {
+      type: 'js/script/virtual',
+      data: {
+        code: debugIdCode,
+        lineCount: countLines(debugIdCode),
+        map: [],
+      },
+    },
+  ],
+};
 
 /**
  * Metro configuration
@@ -40,6 +62,18 @@ const config = {
       },
     ),
   },
+  serializer: {
+    customSerializer: (entryPoint, preModules, graph, options) => {
+      console.log('Adding debugId module...');
+      preModules.unshift(debugIdModule);
+      const bundle = baseJSBundle(entryPoint, preModules, graph, options);
+      return bundleToString(bundle).code;
+    },
+  },
 };
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+const m = mergeConfig(getDefaultConfig(__dirname), config);
+// m.transformer.getTransformOptions().then(opts => {
+//   console.log(opts);
+// });
+module.exports = m;
