f1d91dce8d4e2246200fb4677faded4484d52b5b
Krystof Woldrich <krystof.woldrich@sentry.io>
2023-07-10 15:44:56 +0200

    Add android CI tests

--- RNSentryAndroidTester/app/src/test/java/io/sentry/rnsentryandroidtester/MapConverterUnitTest.kt
+++ RNSentryAndroidTester/app/src/test/java/io/sentry/rnsentryandroidtester/MapConverterUnitTest.kt
@@ -4,16 +4,14 @@ import org.junit.Test
 
 import org.junit.Assert.*
 
-import io.sentry.react.RNSentryModuleImpl;
-
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
+class MapConverterUnitTest {
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        assertEquals(4, 2 + 6)
     }
 }
