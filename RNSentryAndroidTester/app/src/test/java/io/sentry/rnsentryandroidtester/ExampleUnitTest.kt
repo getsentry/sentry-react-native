a867f8ad30c54184988c2988a9fd893967520e39
Krystof Woldrich <krystof.woldrich@sentry.io>
2023-07-10 14:24:12 +0200

    Add RNSentryAndroidTester project to test the RNSentry Android module

---
+++ RNSentryAndroidTester/app/src/test/java/io/sentry/rnsentryandroidtester/ExampleUnitTest.kt
@@ -0,0 +1,19 @@
+package io.sentry.rnsentryandroidtester
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+import io.sentry.react.RNSentryModuleImpl;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
