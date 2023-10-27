55eb480f9ebb68d001fbae4a16ecb247167fdb68
Krystof Woldrich <krystof.woldrich@sentry.io>
2023-10-27 21:36:35 +0200

    check that __DEV__ is defined

--- src/js/utils/environment.ts
+++ src/js/utils/environment.ts
@@ -38,5 +38,5 @@ export function getHermesVersion(): string | undefined {
 
 /** Returns default environment based on __DEV__ */
 export function getDefaultEnvironment(): 'development' | 'production' {
-  return __DEV__ ? 'development' : 'production';
+  return typeof __DEV__ !== 'undefined' && __DEV__ ? 'development' : 'production';
 }
