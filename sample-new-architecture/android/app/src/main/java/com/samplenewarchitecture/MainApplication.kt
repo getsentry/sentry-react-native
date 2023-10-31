73a377418f40b9154798a0ac648cd6892d589b53
Krystof Woldrich <krystof.woldrich@sentry.io>
2023-10-31 11:40:01 +0100

    Upgrade android to 0.73 (not all libs are compatible yet)

---
+++ sample-new-architecture/android/app/src/main/java/com/samplenewarchitecture/MainApplication.kt
@@ -0,0 +1,50 @@
+package com.samplenewarchitecture
+
+import android.app.Application
+import com.facebook.react.PackageList
+import com.facebook.react.ReactApplication
+import com.facebook.react.ReactHost
+import com.facebook.react.ReactNativeHost
+import com.facebook.react.ReactPackage
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
+import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
+import com.facebook.react.defaults.DefaultReactNativeHost
+import com.facebook.react.flipper.ReactNativeFlipper
+import com.facebook.soloader.SoLoader
+import io.sentry.react.RNSentryPackage
+
+class MainApplication : Application(), ReactApplication {
+    override val reactNativeHost: ReactNativeHost =
+        object : DefaultReactNativeHost(this) {
+            override fun getPackages(): List<ReactPackage> {
+                val packages: MutableList<ReactPackage> = PackageList(this).packages
+                packages.add(SamplePackage())
+                // Packages that cannot be auto linked yet can be added manually here, for example:
+                // packages.add(new MyReactNativePackage());
+                for (pkg in packages) {
+                    if (pkg is RNSentryPackage) {
+                        return packages
+                    }
+                }
+                packages.add(RNSentryPackage())
+                return packages
+            }
+            override fun getJSMainModuleName(): String = "index"
+            override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+            override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+            override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+        }
+
+    override val reactHost: ReactHost
+        get() = getDefaultReactHost(this.applicationContext, reactNativeHost)
+
+    override fun onCreate() {
+        super.onCreate()
+        SoLoader.init(this, false)
+        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+            // If you opted-in for the New Architecture, we load the native entry point for this app.
+            load()
+        }
+        ReactNativeFlipper.initializeFlipper(this, reactNativeHost.reactInstanceManager)
+    }
+}
