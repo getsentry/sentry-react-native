b9c1c2c4a1cd2dc973abd4c883e40e605506032a
Lucas <lucas-zimerman1@hotmail.com>
2023-11-20 15:02:26 -0300

    update the update script for sentry libraries

--- scripts/update-javascript.sh
+++ scripts/update-javascript.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 tagPrefix=''
 repo="https://github.com/getsentry/sentry-javascript.git"
-packages=('@sentry/browser' '@sentry/core' '@sentry/hub' '@sentry/integrations' '@sentry/react' '@sentry/types' '@sentry/utils')
+packages=('@sentry/browser' '@sentry/core' '@sentry/hub' '@sentry/integrations' '@sentry/react' '@sentry-internal/types' '@sentry/utils')
 packages+=('@sentry-internal/eslint-config-sdk' '@sentry-internal/eslint-plugin-sdk')
 
 . $(dirname "$0")/update-package-json.sh
