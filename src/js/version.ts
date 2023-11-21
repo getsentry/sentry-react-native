95f6403bda2e373633db6c546869c0dacd685cf8
Kryštof Woldřich <31292499+krystofwoldrich@users.noreply.github.com>
2023-11-21 17:51:21 +0100

    Update version.ts

--- src/js/version.ts
+++ src/js/version.ts
@@ -1,3 +1,3 @@
 export const SDK_PACKAGE_NAME = 'npm:@sentry/react-native';
 export const SDK_NAME = 'sentry.javascript.react-native';
-export const SDK_VERSION = '5.13.1-beta.1';
+export const SDK_VERSION = '5.13.0';
