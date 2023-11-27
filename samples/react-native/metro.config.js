abc65df6f15d0e1750bdfad603fab1f050263948
Krystof Woldrich <krystof.woldrich@sentry.io>
2023-11-27 23:07:26 +0100

    chore: Move `sample-new-arch` to `samples/react-native`

--- samples/react-native/metro.config.js
+++ samples/react-native/metro.config.js
@@ -4,7 +4,7 @@ const blacklist = require('metro-config/src/defaults/exclusionList');
 
 const {
   createSentryMetroSerializer,
-} = require('../dist/js/tools/sentryMetroSerializer');
+} = require('../../dist/js/tools/sentryMetroSerializer');
 const parentDir = path.resolve(__dirname, '..');
 
 /**
