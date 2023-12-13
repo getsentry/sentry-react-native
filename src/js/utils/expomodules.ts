c475c852376c5d1e7967aa0c9d544f53b55360e7
Krystof Woldrich <krystof.woldrich@sentry.io>
2023-12-13 12:19:54 +0100

    Add expo modules getters js docs

--- src/js/utils/expomodules.ts
+++ src/js/utils/expomodules.ts
@@ -2,7 +2,7 @@ import type { ExpoConstants, ExpoDevice } from './expoglobalobject';
 import { RN_GLOBAL_OBJ } from './worldwide';
 
 /**
- *
+ * Returns the Expo Constants module if present
  */
 export function getExpoConstants(): ExpoConstants | undefined {
   return (
@@ -11,7 +11,7 @@ export function getExpoConstants(): ExpoConstants | undefined {
 }
 
 /**
- *
+ * Returns the Expo Device module if present
  */
 export function getExpoDevice(): ExpoDevice | undefined {
   return (RN_GLOBAL_OBJ.expo && RN_GLOBAL_OBJ.expo.modules && RN_GLOBAL_OBJ.expo.modules.ExpoDevice) || undefined;
