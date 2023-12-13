e55b180a4293a64e8096b9196eac78e31b802133
Krystof Woldrich <krystof.woldrich@sentry.io>
2023-12-13 10:53:50 +0100

    feat(expo): Add Expo Modules Integration

---
+++ src/js/utils/expoglobalobject.ts
@@ -0,0 +1,49 @@
+/**
+ * Interface from the Expo SDK defined here
+ * (we are describing the native Module
+ * the TS typing is only guideline):
+ *
+ * https://github.com/expo/expo/blob/5d1153e6ae7c497fa1281ffee85fabe90d2321c2/packages/expo-constants/src/Constants.types.ts#L124
+ */
+export interface ExpoConstants {
+  appOwnership?: 'standalone' | 'expo' | 'guest';
+  /**
+   * Deprecated. But until removed we can use it as user ID to match the native SDKs.
+   */
+  installationId?: string;
+  /**
+   * Version of the Expo Go app
+   */
+  expoVersion?: string | null;
+  expoConfig?: null | {
+    [key: string]: unknown;
+    /**
+     * Expo SDK version should match `expo` version from the app `package.json`.
+     */
+    sdkVersion?: string;
+  };
+}
+
+/**
+ * Interface from the Expo SDK defined here
+ * (we are describing the native module
+ * the TS typing is only guideline)
+ *
+ * https://github.com/expo/expo/blob/5d1153e6ae7c497fa1281ffee85fabe90d2321c2/packages/expo-device/src/Device.ts
+ */
+export interface ExpoDevice {
+  deviceName?: string;
+  isDevice?: boolean;
+  manufacturer?: string;
+  modelName?: string;
+  osBuildId?: string;
+  osVersion?: string;
+  totalMemory?: number;
+}
+
+export interface ExpoGlobalObject {
+  modules?: {
+    ExponentConstants?: ExpoConstants
+    ExpoDevice?: ExpoDevice;
+  };
+}
---
+++ src/js/utils/expomodules.ts
@@ -0,0 +1,14 @@
+import { ExpoConstants, ExpoDevice } from './expoglobalobject';
+import { RN_GLOBAL_OBJ } from './worldwide';
+
+export function getExpoConstants(): ExpoConstants | undefined {
+  return (RN_GLOBAL_OBJ.expo
+    && RN_GLOBAL_OBJ.expo.modules
+    && RN_GLOBAL_OBJ.expo.modules.ExponentConstants) || undefined;
+}
+
+export function getExpoDevice(): ExpoDevice | undefined {
+  return (RN_GLOBAL_OBJ.expo
+    && RN_GLOBAL_OBJ.expo.modules
+    && RN_GLOBAL_OBJ.expo.modules.ExpoDevice) || undefined;
+}
