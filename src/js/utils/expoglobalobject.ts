eb26127584a4a657838222e42b132fbda704b3f7
Kryštof Woldřich <31292499+krystofwoldrich@users.noreply.github.com>
2024-01-09 18:18:06 +0100

    Update type source links

--- src/js/utils/expoglobalobject.ts
+++ src/js/utils/expoglobalobject.ts
@@ -3,7 +3,8 @@
  * (we are describing the native Module
  * the TS typing is only guideline):
  *
- * https://github.com/expo/expo/blob/5d1153e6ae7c497fa1281ffee85fabe90d2321c2/packages/expo-constants/src/Constants.types.ts#L124
+ * https://github.com/expo/expo/blob/b51b5139f2caa2a9495e4132437d7ca612276158/packages/expo-constants/src/Constants.ts
+ * https://github.com/expo/expo/blob/b51b5139f2caa2a9495e4132437d7ca612276158/packages/expo-manifests/src/Manifests.ts
  */
 export interface ExpoConstants {
   appOwnership?: 'standalone' | 'expo' | 'guest';
