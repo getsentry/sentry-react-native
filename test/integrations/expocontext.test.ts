19c9c245cebda8c282a89260122b2db8606d156e
Krystof Woldrich <krystof.woldrich@sentry.io>
2024-01-09 17:03:52 +0100

    WIP! Add Expo context integration tests

---
+++ test/integrations/expocontext.test.ts
@@ -0,0 +1,53 @@
+import type { Hub } from '@sentry/core';
+import type { Event } from '@sentry/types';
+
+import { ExpoContext } from '../../src/js/integrations/expocontext';
+import { getExpoDevice } from '../../src/js/utils/expomodules';
+
+jest.mock('../../src/js/utils/expomodules');
+
+describe('Expo Context Integration', () => {
+  let integration: ExpoContext;
+
+  const mockGetCurrentHub = () =>
+    ({
+      getIntegration: () => integration,
+    } as unknown as Hub);
+
+  beforeEach(() => {
+    integration = new ExpoContext();
+  });
+
+  it('does not add device context because expo device module is not available', async () => {
+    (getExpoDevice as jest.Mock).mockReturnValue(undefined);
+    const actualEvent = await executeIntegrationFor({});
+
+    expect(actualEvent.contexts?.device).toBeUndefined();
+  });
+
+  it('does not add os context because expo device module is not available', async () => {
+    (getExpoDevice as jest.Mock).mockReturnValue(undefined);
+    const actualEvent = await executeIntegrationFor({});
+
+    expect(actualEvent.contexts?.os).toBeUndefined();
+  });
+
+  it('adds expo device context', async () => {
+
+  });
+
+  it('adds expo os context', async () => { });
+
+  function executeIntegrationFor(mockedEvent: Event): Promise<Event | null> {
+    return new Promise((resolve, reject) => {
+      integration.setupOnce(async eventProcessor => {
+        try {
+          const processedEvent = await eventProcessor(mockedEvent, {});
+          resolve(processedEvent);
+        } catch (e) {
+          reject(e);
+        }
+      }, mockGetCurrentHub);
+    });
+  }
+});
