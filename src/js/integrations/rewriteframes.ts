73ce84804a21d217f32ccb195d8effce2f804283
lucas <lucas-zimerman1@hotmail.com>
2024-01-11 00:22:02 -0300

    test

--- src/js/integrations/rewriteframes.ts
+++ src/js/integrations/rewriteframes.ts
@@ -57,7 +57,10 @@ export function createReactNativeRewriteFrames(): RewriteFrames {
       }
 
       const appPrefix = 'app://';
-      // We always want to have a triple slash
+      // https://github.com/getsentry/sentry-react-native/issues/3348
+      if (frame.filename === '/InternalBytecode.js') {
+        frame.in_app = false;
+      }      // We always want to have a triple slash
       frame.filename =
         frame.filename.indexOf('/') === 0 ? `${appPrefix}${frame.filename}` : `${appPrefix}/${frame.filename}`;
       return frame;
