31ae55c3125aa96692c81e92c0bff50f3c1e0c96
Quinlan Jung <quin@expo.io>
2024-01-29 21:11:46 -0800

    nits

--- scripts/expo-upload-sourcemaps.js
+++ scripts/expo-upload-sourcemaps.js
@@ -168,7 +168,7 @@ if (numAssetsUploaded === totalAssets) {
   console.log('✅ Uploaded bundles and sourcemaps to Sentry successfully.');
 } else {
   console.warn(
-    `⚠️  Uploaded ${numAssetsUploaded} of ${totalAssets} assets. ${
+    `⚠️  Uploaded ${numAssetsUploaded} of ${totalAssets} bundles and sourcemaps. ${
       numAssetsUploaded === 0 ? 'Ensure you are running `expo export` with the `--dump-sourcemap` flag.' : ''
     }`,
   );
