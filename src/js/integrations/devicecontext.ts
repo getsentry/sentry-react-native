0b1d5eef68464824f53bf21b905686c0594a70e8
Krystof Woldrich <krystof.woldrich@sentry.io>
2024-02-01 17:56:18 +0100

    fix device context

--- src/js/integrations/devicecontext.ts
+++ src/js/integrations/devicecontext.ts
@@ -21,80 +21,81 @@ export class DeviceContext implements Integration {
 
   /**
    * @inheritDoc
+   * @deprecated
    */
-  public setupOnce(addGlobalEventProcessor: (callback: EventProcessor) => void, getCurrentHub: () => Hub): void {
-    addGlobalEventProcessor(async (event: Event) => {
-      const self = getCurrentHub().getIntegration(DeviceContext);
-      if (!self) {
-        return event;
-      }
+  public setupOnce(_addGlobalEventProcessor: (callback: EventProcessor) => void, _getCurrentHub: () => Hub): void {
+    // nothing to do here
+  }
 
-      let native: NativeDeviceContextsResponse | null = null;
-      try {
-        native = await NATIVE.fetchNativeDeviceContexts();
-      } catch (e) {
-        logger.log(`Failed to get device context from native: ${e}`);
-      }
+  /**
+   * @inheritDoc
+   */
+  public async processEvent(event: Event): Promise<Event> {
+    let native: NativeDeviceContextsResponse | null = null;
+    try {
+      native = await NATIVE.fetchNativeDeviceContexts();
+    } catch (e) {
+      logger.log(`Failed to get device context from native: ${e}`);
+    }
 
-      if (!native) {
-        return event;
-      }
-
-      const nativeUser = native.user;
-      if (!event.user && nativeUser) {
-        event.user = nativeUser;
-      }
-
-      let nativeContexts = native.contexts;
-      if (AppState.currentState !== 'unknown') {
-        nativeContexts = nativeContexts || {};
-        nativeContexts.app = {
-          ...nativeContexts.app,
-          in_foreground: AppState.currentState === 'active',
-        };
-      }
-      if (nativeContexts) {
-        event.contexts = { ...nativeContexts, ...event.contexts };
-        if (nativeContexts.app) {
-          event.contexts.app = { ...nativeContexts.app, ...event.contexts.app };
-        }
-      }
-
-      const nativeTags = native.tags;
-      if (nativeTags) {
-        event.tags = { ...nativeTags, ...event.tags };
-      }
-
-      const nativeExtra = native.extra;
-      if (nativeExtra) {
-        event.extra = { ...nativeExtra, ...event.extra };
-      }
-
-      const nativeFingerprint = native.fingerprint;
-      if (nativeFingerprint) {
-        event.fingerprint = (event.fingerprint ?? []).concat(
-          nativeFingerprint.filter(item => (event.fingerprint ?? []).indexOf(item) < 0),
-        );
-      }
-
-      const nativeLevel = typeof native['level'] === 'string' ? severityLevelFromString(native['level']) : undefined;
-      if (!event.level && nativeLevel) {
-        event.level = nativeLevel;
-      }
-
-      const nativeEnvironment = native['environment'];
-      if (!event.environment && nativeEnvironment) {
-        event.environment = nativeEnvironment;
-      }
-
-      const nativeBreadcrumbs = Array.isArray(native['breadcrumbs'])
-        ? native['breadcrumbs'].map(breadcrumbFromObject)
-        : undefined;
-      if (nativeBreadcrumbs) {
-        event.breadcrumbs = nativeBreadcrumbs;
-      }
-
+    if (!native) {
       return event;
-    });
+    }
+
+    const nativeUser = native.user;
+    if (!event.user && nativeUser) {
+      event.user = nativeUser;
+    }
+
+    let nativeContexts = native.contexts;
+    if (AppState.currentState !== 'unknown') {
+      nativeContexts = nativeContexts || {};
+      nativeContexts.app = {
+        ...nativeContexts.app,
+        in_foreground: AppState.currentState === 'active',
+      };
+    }
+    if (nativeContexts) {
+      event.contexts = { ...nativeContexts, ...event.contexts };
+      if (nativeContexts.app) {
+        event.contexts.app = { ...nativeContexts.app, ...event.contexts.app };
+      }
+    }
+
+    const nativeTags = native.tags;
+    if (nativeTags) {
+      event.tags = { ...nativeTags, ...event.tags };
+    }
+
+    const nativeExtra = native.extra;
+    if (nativeExtra) {
+      event.extra = { ...nativeExtra, ...event.extra };
+    }
+
+    const nativeFingerprint = native.fingerprint;
+    if (nativeFingerprint) {
+      event.fingerprint = (event.fingerprint ?? []).concat(
+        nativeFingerprint.filter(item => (event.fingerprint ?? []).indexOf(item) < 0),
+      );
+    }
+
+    const nativeLevel = typeof native['level'] === 'string' ? severityLevelFromString(native['level']) : undefined;
+    if (!event.level && nativeLevel) {
+      event.level = nativeLevel;
+    }
+
+    const nativeEnvironment = native['environment'];
+    if (!event.environment && nativeEnvironment) {
+      event.environment = nativeEnvironment;
+    }
+
+    const nativeBreadcrumbs = Array.isArray(native['breadcrumbs'])
+      ? native['breadcrumbs'].map(breadcrumbFromObject)
+      : undefined;
+    if (nativeBreadcrumbs) {
+      event.breadcrumbs = nativeBreadcrumbs;
+    }
+
+    return event;
   }
 }
