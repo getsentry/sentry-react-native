02a6d2b5583bb4634787c79ad0d591d202ececf2
Krystof Woldrich <krystof.woldrich@sentry.io>
2024-02-02 10:28:56 +0100

    fix release

--- src/js/integrations/release.ts
+++ src/js/integrations/release.ts
@@ -1,5 +1,4 @@
-import { addGlobalEventProcessor, getCurrentHub } from '@sentry/core';
-import type { Event, Integration } from '@sentry/types';
+import type { BaseTransportOptions, Client, ClientOptions, Event, EventHint, Integration } from '@sentry/types';
 
 import { NATIVE } from '../wrapper';
 
@@ -16,51 +15,52 @@ export class Release implements Integration {
 
   /**
    * @inheritDoc
+   * @deprecated
    */
   public setupOnce(): void {
-    addGlobalEventProcessor(async (event: Event) => {
-      const self = getCurrentHub().getIntegration(Release);
-      if (!self) {
-        return event;
-      }
+    // nothing to do here
+  }
 
-      const options = getCurrentHub().getClient()?.getOptions();
+  /**
+   * @inheritDoc
+   */
+  public async processEvent(event: Event, _: EventHint, client: Client<ClientOptions<BaseTransportOptions>>): Promise<Event> {
+    const options = client.getOptions();
 
-      /*
-        __sentry_release and __sentry_dist is set by the user with setRelease and setDist. If this is used then this is the strongest.
-        Otherwise we check for the release and dist in the options passed on init, as this is stronger than the release/dist from the native build.
-      */
-      if (typeof event.extra?.__sentry_release === 'string') {
-        event.release = `${event.extra.__sentry_release}`;
-      } else if (typeof options?.release === 'string') {
-        event.release = options.release;
-      }
+    /*
+      __sentry_release and __sentry_dist is set by the user with setRelease and setDist. If this is used then this is the strongest.
+      Otherwise we check for the release and dist in the options passed on init, as this is stronger than the release/dist from the native build.
+    */
+    if (typeof event.extra?.__sentry_release === 'string') {
+      event.release = `${event.extra.__sentry_release}`;
+    } else if (typeof options?.release === 'string') {
+      event.release = options.release;
+    }
 
-      if (typeof event.extra?.__sentry_dist === 'string') {
-        event.dist = `${event.extra.__sentry_dist}`;
-      } else if (typeof options?.dist === 'string') {
-        event.dist = options.dist;
-      }
+    if (typeof event.extra?.__sentry_dist === 'string') {
+      event.dist = `${event.extra.__sentry_dist}`;
+    } else if (typeof options?.dist === 'string') {
+      event.dist = options.dist;
+    }
 
-      if (event.release && event.dist) {
-        return event;
-      }
-
-      try {
-        const nativeRelease = await NATIVE.fetchNativeRelease();
-        if (nativeRelease) {
-          if (!event.release) {
-            event.release = `${nativeRelease.id}@${nativeRelease.version}+${nativeRelease.build}`;
-          }
-          if (!event.dist) {
-            event.dist = `${nativeRelease.build}`;
-          }
-        }
-      } catch (_Oo) {
-        // Something went wrong, we just continue
-      }
-
+    if (event.release && event.dist) {
       return event;
-    });
+    }
+
+    try {
+      const nativeRelease = await NATIVE.fetchNativeRelease();
+      if (nativeRelease) {
+        if (!event.release) {
+          event.release = `${nativeRelease.id}@${nativeRelease.version}+${nativeRelease.build}`;
+        }
+        if (!event.dist) {
+          event.dist = `${nativeRelease.build}`;
+        }
+      }
+    } catch (_Oo) {
+      // Something went wrong, we just continue
+    }
+
+    return event;
   }
 }
