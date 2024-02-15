32598e3bc5b906400dca843bf092452c329dac2c
Krystof Woldrich <krystof.woldrich@sentry.io>
2024-02-15 10:21:42 +0100

    remove unused header file

--- samples/react-native/ios/sampleNewArchitecture/TurboCrashModule.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-
-#ifdef RCT_NEW_ARCH_ENABLED
-#import <AppSpecs/AppSpecs.h>
-
-@interface NativePlatformSampleModule : NSObject <NativePlatformSampleModuleSpec>
-
-@end
-#endif
