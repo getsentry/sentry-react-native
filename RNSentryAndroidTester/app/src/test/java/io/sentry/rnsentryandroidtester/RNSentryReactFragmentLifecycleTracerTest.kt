24f5d31f1b2b825441f6dd0879a83ef601a5b146
Krystof Woldrich <krystof.woldrich@sentry.io>
2024-02-21 15:23:49 +0100

    fix java test

--- RNSentryAndroidTester/app/src/test/java/io/sentry/rnsentryandroidtester/RNSentryReactFragmentLifecycleTracerTest.kt
+++ RNSentryAndroidTester/app/src/test/java/io/sentry/rnsentryandroidtester/RNSentryReactFragmentLifecycleTracerTest.kt
@@ -5,25 +5,26 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.facebook.react.bridge.ReactContext
 import com.facebook.react.uimanager.UIManagerHelper
 import com.facebook.react.uimanager.events.EventDispatcher
+import com.swmansion.rnscreens.ScreenStackFragment
 import io.sentry.ILogger
-import io.sentry.android.core.AndroidLogger
 import io.sentry.android.core.BuildInfoProvider
 import io.sentry.react.RNSentryModuleImpl
 import io.sentry.react.RNSentryReactFragmentLifecycleTracer
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mockito
+import org.junit.runners.JUnit4
+import org.mockito.ArgumentMatchers.any
+import org.mockito.ArgumentMatchers.anyInt
 import org.mockito.Mockito.mockStatic
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.times
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 
-@RunWith(AndroidJUnit4::class)
+@RunWith(JUnit4::class)
 class RNSentryReactFragmentLifecycleTracerTest {
 
     @Test
@@ -32,7 +33,7 @@ class RNSentryReactFragmentLifecycleTracerTest {
         val sut = createSutWith()
         callOnFragmentViewCreated(sut, mockRnScreensFragment(), mockValidView())
 
-        verify(mockEventDispatcher, times(1)).addListener(Mockito.any())
+        verify(mockEventDispatcher, times(1)).addListener(any())
     }
 
     private fun callOnFragmentViewCreated(sut: RNSentryReactFragmentLifecycleTracer, mockFragment: Fragment, mockView: View) {
@@ -48,7 +49,7 @@ class RNSentryReactFragmentLifecycleTracerTest {
     }
 
     private fun createSutWith(): RNSentryReactFragmentLifecycleTracer {
-        val logger: ILogger = AndroidLogger(RNSentryModuleImpl.NAME)
+        val logger: ILogger = mock()
         val buildInfo = BuildInfoProvider(logger)
 
         return RNSentryReactFragmentLifecycleTracer(
@@ -59,17 +60,18 @@ class RNSentryReactFragmentLifecycleTracerTest {
     }
 
     private fun mockRnScreensFragment(): Fragment {
-        val mockFragment: Fragment = mock()
-        val mockFragmentClass: Class<Fragment> = mock()
-        whenever(mockFragmentClass.canonicalName).thenReturn("com.swmansion.rnscreens.ScreenStackFragment")
-        whenever(mockFragment.javaClass).thenReturn(mockFragmentClass)
-
-        return mockFragment
+        return mock<ScreenStackFragment>()
     }
 
     private fun mockValidView(): View {
-        val mockView = mock<ViewGroup>();
-
+        val screenMock: View = mock() {
+            whenever(it.id).thenReturn(123)
+            whenever(it.context).thenReturn(mock<ReactContext>())
+        }
+        val mockView = mock<ViewGroup> {
+            whenever(it.childCount).thenReturn(1)
+            whenever(it.getChildAt(0)).thenReturn(screenMock)
+        }
         return mockView;
     }
 
@@ -77,10 +79,10 @@ class RNSentryReactFragmentLifecycleTracerTest {
         val mockEventDispatcher = mock<EventDispatcher>()
         val mockUIManager = mockStatic(UIManagerHelper::class.java)
         mockUIManager
-            .`when`<ReactContext> { UIManagerHelper.getReactContext(Mockito.any()) }
+            .`when`<ReactContext> { UIManagerHelper.getReactContext(any()) }
             .thenReturn(mock())
         mockUIManager
-            .`when`<EventDispatcher> { UIManagerHelper.getEventDispatcherForReactTag(Mockito.any(), Mockito.any()) }
+            .`when`<EventDispatcher> { UIManagerHelper.getEventDispatcherForReactTag(any(), anyInt()) }
             .thenReturn(mockEventDispatcher)
         return mockEventDispatcher
     }
