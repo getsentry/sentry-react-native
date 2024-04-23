8a291ed627b8591671f10e601bea3895d21f92de
Krystof Woldrich <krystof.woldrich@sentry.io>
2024-04-23 16:12:30 +0200

    Add on error sample rate to the sample app

--- samples/react-native/src/App.tsx
+++ samples/react-native/src/App.tsx
@@ -101,7 +101,8 @@ Sentry.init({
     profilesSampleRate: 1.0,
   },
   enableSpotlight: true,
-  replaysSessionSampleRate: 1.0,
+  // replaysSessionSampleRate: 1.0,
+  replaysOnErrorSampleRate: 1.0,
 });
 
 const Stack = createNativeStackNavigator();
