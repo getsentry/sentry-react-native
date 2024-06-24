1a13827777c3cf54eedf45e62db24ecd6bba6cc3
Ivan Dlugos <dlugos.ivan@gmail.com>
2024-06-24 11:28:24 +0200

    more updates based on upstream changes

--- ios/RNSentryBreadcrumbConverter.h
+++ ios/RNSentryBreadcrumbConverter.h
@@ -9,10 +9,5 @@
 
 - (instancetype _Nonnull)init;
 
-- (NSArray<SentryRRWebEvent *> *_Nonnull)
-    convertWithBreadcrumbs:(NSArray<SentryBreadcrumb *> *_Nonnull)breadcrumbs
-                      from:(NSDate *_Nonnull)from
-                     until:(NSDate *_Nonnull)until;
-
 @end
 #endif
--- ios/RNSentryBreadcrumbConverter.m
+++ ios/RNSentryBreadcrumbConverter.m
@@ -17,35 +17,10 @@
   return self;
 }
 
-- (NSArray<SentryRRWebEvent *> *_Nonnull)
-    convertWithBreadcrumbs:(NSArray<SentryBreadcrumb *> *_Nonnull)breadcrumbs
-                      from:(NSDate *_Nonnull)from
-                     until:(NSDate *_Nonnull)until {
-  NSMutableArray<SentryRRWebEvent *> *outBreadcrumbs = [NSMutableArray array];
-  for (SentryBreadcrumb *breadcrumb in breadcrumbs) {
-    // - (NSComparisonResult)compare:(NSDate *)other;
-    // If:
-    // The receiver and `other` are exactly equal to each other, NSOrderedSame
-    // The receiver is later in time than `other`, NSOrderedDescending
-    // The receiver is earlier in time than `other`, NSOrderedAscending.
-    if (!breadcrumb.timestamp ||
-        [breadcrumb.timestamp compare:from] == NSOrderedAscending ||
-        [breadcrumb.timestamp compare:until] == NSOrderedDescending) {
-      continue;
-    }
-
-    SentryRRWebEvent *rrwebBreadcrumb = [self convertFrom:breadcrumb];
-    if (rrwebBreadcrumb) {
-      [outBreadcrumbs addObject:rrwebBreadcrumb];
-    }
-  }
-  return outBreadcrumbs;
-}
-
 - (id<SentryRRWebEvent> _Nullable)convertFrom:
     (SentryBreadcrumb *_Nonnull)breadcrumb {
-    assert(breadcrumb.timestamp != nil);
-    
+  assert(breadcrumb.timestamp != nil);
+
   if ([breadcrumb.category isEqualToString:@"touch"]) {
     return [SentrySessionReplayIntegration
         createBreadcrumbwithTimestamp:breadcrumb.timestamp
@@ -58,7 +33,7 @@
                                  data:breadcrumb.data];
   } else if ([breadcrumb.category isEqualToString:@"navigation"]) {
     return [SentrySessionReplayIntegration
-            createBreadcrumbwithTimestamp:breadcrumb.timestamp ?: 0
+        createBreadcrumbwithTimestamp:breadcrumb.timestamp ?: 0
                              category:breadcrumb.category
                               message:nil
                                 level:breadcrumb.level
