6af7578cfcf4b8bd84bdf0056ed51134d477b947
Krystof Woldrich <31292499+krystofwoldrich@users.noreply.github.com>
2024-06-25 16:25:07 +0200

    fix(scope): Hooks into Global and Isolations Scopes for Sync with the native scope (#3911)

--- src/js/sdk.tsx
+++ src/js/sdk.tsx
@@ -1,10 +1,10 @@
 /* eslint-disable complexity */
-import { getClient, getIntegrationsToSetup, initAndBind, withScope as coreWithScope } from '@sentry/core';
+import { getClient, getGlobalScope,getIntegrationsToSetup, getIsolationScope,initAndBind, withScope as coreWithScope } from '@sentry/core';
 import {
   defaultStackParser,
   makeFetchTransport,
 } from '@sentry/react';
-import type { Integration, Scope, UserFeedback } from '@sentry/types';
+import type { Integration, Scope,UserFeedback } from '@sentry/types';
 import { logger, stackParserFromStackParserOptions } from '@sentry/utils';
 import * as React from 'react';
 
@@ -12,6 +12,7 @@ import { ReactNativeClient } from './client';
 import { getDefaultIntegrations } from './integrations/default';
 import type { ReactNativeClientOptions, ReactNativeOptions, ReactNativeWrapperOptions } from './options';
 import { shouldEnableNativeNagger } from './options';
+import { enableSyncToNative } from './scopeSync';
 import { TouchEventBoundary } from './touchevents';
 import type { ReactNativeTracing } from './tracing';
 import { ReactNativeProfiler } from './tracing';
@@ -43,8 +44,6 @@ export function init(passedOptions: ReactNativeOptions): void {
     return;
   }
 
-  useEncodePolyfill();
-
   const maxQueueSize = passedOptions.maxQueueSize
     // eslint-disable-next-line deprecation/deprecation
     ?? passedOptions.transportOptions?.bufferSize
@@ -53,6 +52,13 @@ export function init(passedOptions: ReactNativeOptions): void {
   const enableNative = passedOptions.enableNative === undefined || passedOptions.enableNative
     ? NATIVE.isNativeAvailable()
     : false;
+
+  useEncodePolyfill();
+  if (enableNative) {
+    enableSyncToNative(getGlobalScope());
+    enableSyncToNative(getIsolationScope());
+  }
+
   const options: ReactNativeClientOptions = {
     ...DEFAULT_OPTIONS,
     ...passedOptions,
