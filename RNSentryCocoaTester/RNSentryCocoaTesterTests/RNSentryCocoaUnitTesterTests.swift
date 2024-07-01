c80322a38c149628b3634ae52495733ecaa5029f
Krystof Woldrich <krystof.woldrich@sentry.io>
2024-07-01 17:20:31 +0200

    add swift tests

---
+++ RNSentryCocoaTester/RNSentryCocoaTesterTests/RNSentryCocoaUnitTesterTests.swift
@@ -0,0 +1,52 @@
+import XCTest
+
+final class RNSentryCocoaUnitTesterTests: XCTestCase {
+
+    func testTouchMessageReturnsNilOnEmptyArray() throws {
+        let converter = RNSentryReplayBreadcrumbConverter()
+      let actual = converter.getTouchPathMessage(from: [])
+        XCTAssertEqual(actual, nil);
+    }
+
+    func testTouchMessageReturnsNilOnNilArray() throws {
+        let converter = RNSentryReplayBreadcrumbConverter()
+      let actual = converter.getTouchPathMessage(from: nil as [Any]?)
+        XCTAssertEqual(actual, nil);
+    }
+  
+    func testTouchMessageReturnsNilOnMissingNameAndLavel() throws {
+        let testPath: [Any?] = [["element": "element4", "file": "file4"]]
+        let converter = RNSentryReplayBreadcrumbConverter()
+        let actual = converter.getTouchPathMessage(from: testPath as [Any])
+        XCTAssertEqual(actual, nil);
+    }
+  
+    func testTouchMessageReturnsMessageOnValidPathExample1() throws {
+        let testPath: [Any?] = [
+            ["label": "label0"],
+            ["name": "name1"],
+            ["name": "item2", "label": "label2"],
+            ["name": "item3", "label": "label3", "element": "element3"],
+            ["name": "item4", "label": "label4", "file": "file4"],
+            ["name": "item5", "label": "label5", "element": "element5", "file": "file5"],
+        ]
+        let converter = RNSentryReplayBreadcrumbConverter()
+        let actual = converter.getTouchPathMessage(from: testPath as [Any])
+        XCTAssertEqual(actual, "label3(element3) > label2 > name1 > label0");
+    }
+  
+    func testTouchMessageReturnsMessageOnValidPathExample2() throws {
+        let testPath: [Any?] = [
+            ["name": "item2", "label": "label2"],
+            ["name": "item3", "label": "label3", "element": "element3"],
+            ["name": "item4", "label": "label4", "file": "file4"],
+            ["name": "item5", "label": "label5", "element": "element5", "file": "file5"],
+            ["label": "label6"],
+            ["name": "name7"],
+        ]
+        let converter = RNSentryReplayBreadcrumbConverter()
+        let actual = converter.getTouchPathMessage(from: testPath as [Any])
+        XCTAssertEqual(actual, "label5(element5, file5) > label4(file4) > label3(element3) > label2");
+    }
+
+}
--- ios/RNSentryReplayBreadcrumbConverter.m
+++ ios/RNSentryReplayBreadcrumbConverter.m
@@ -67,7 +67,7 @@
   }
   
   NSMutableArray *path = [breadcrumb.data valueForKey:@"path"];
-  NSString* message = [self getMessageFrom:path];
+  NSString* message = [self getTouchPathMessageFrom:path];
 
   return [SentrySessionReplayIntegration
       createBreadcrumbwithTimestamp:breadcrumb.timestamp
@@ -77,7 +77,7 @@
                                data:breadcrumb.data];
 }
 
-- (NSString* _Nullable) getMessageFrom:(NSArray* _Nullable) path {
+- (NSString* _Nullable) getTouchPathMessageFrom:(NSArray* _Nullable) path {
   if (path == nil) {
     return nil;
   }
