bc909c12e742542d5c89a026aa52303358265cd5
Krystof Woldrich <krystof.woldrich@sentry.io>
2024-07-02 18:14:22 +0200

    ensure no nils are added to classes to redact

--- ios/RNSentryReplay.m
+++ ios/RNSentryReplay.m
@@ -41,11 +41,20 @@
 + (void)addReplayRNRedactClasses:(NSDictionary *_Nullable)replayOptions {
   NSMutableArray *_Nonnull classesToRedact = [[NSMutableArray alloc] init];
   if ([replayOptions[@"maskAllImages"] boolValue] == YES) {
-    [classesToRedact addObject:NSClassFromString(@"RCTImageView")];
-    [classesToRedact addObject:NSClassFromString(@"RNSVGSvgView")];
+    Class _Nullable maybeRCTImageClass = NSClassFromString(@"RCTImageView");
+    if (maybeRCTImageClass != nil) {
+      [classesToRedact addObject:maybeRCTImageClass];
+    }
+    Class _Nullable maybeRNSVGViewClass = NSClassFromString(@"RNSVGSvgView");
+    if (maybeRNSVGViewClass != nil) {
+      [classesToRedact addObject:maybeRNSVGViewClass];
+    }
   }
   if ([replayOptions[@"maskAllText"] boolValue] == YES) {
-    [classesToRedact addObject:NSClassFromString(@"RCTTextView")];
+    Class _Nullable maybeRCTTextClass = NSClassFromString(@"RCTTextView");
+    if (maybeRCTTextClass != nil) {
+      [classesToRedact addObject:maybeRCTTextClass];
+    }
   }
   [PrivateSentrySDKOnly addReplayRedactClasses:classesToRedact];
 }
