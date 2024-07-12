3fa2f02e05ec90bcb29eb349e0b1cb26504cad06
Krystof Woldrich <krystof.woldrich@sentry.io>
2024-07-12 11:49:03 +0200

    fix(replay): Add missing properties to android nav breadcrumbs

--- RNSentryAndroidTester/app/src/test/java/io/sentry/rnsentryandroidtester/RNSentryReplayBreadcrumbConverterTest.kt
+++ RNSentryAndroidTester/app/src/test/java/io/sentry/rnsentryandroidtester/RNSentryReplayBreadcrumbConverterTest.kt
@@ -1,8 +1,10 @@
 package io.sentry.rnsentryandroidtester
 
 import io.sentry.Breadcrumb
+import io.sentry.SentryLevel
 import io.sentry.react.RNSentryReplayBreadcrumbConverter
 import io.sentry.rrweb.RRWebBreadcrumbEvent
+import io.sentry.rrweb.RRWebEventType
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -12,7 +14,42 @@ import org.junit.runners.JUnit4
 class RNSentryReplayBreadcrumbConverterTest {
 
     @Test
-    fun testConvertForegroundBreadcrumb() {
+    fun convertNavigationBreadcrumb() {
+        val converter = RNSentryReplayBreadcrumbConverter()
+        val testBreadcrumb = Breadcrumb()
+        testBreadcrumb.level = SentryLevel.INFO
+        testBreadcrumb.type = "navigation"
+        testBreadcrumb.category = "navigation"
+        testBreadcrumb.setData("from", "HomeScreen")
+        testBreadcrumb.setData("to", "ProfileScreen")
+        val actual = converter.convert(testBreadcrumb) as RRWebBreadcrumbEvent
+
+        assertRRWebBreadcrumbDefaults(actual)
+        assertEquals(SentryLevel.INFO, actual.level)
+        assertEquals("navigation", actual.category)
+        assertEquals("HomeScreen", actual.data?.get("from"))
+        assertEquals("ProfileScreen", actual.data?.get("to"))
+    }
+
+    @Test
+    fun convertNavigationBreadcrumbWithOnlyTo() {
+        val converter = RNSentryReplayBreadcrumbConverter()
+        val testBreadcrumb = Breadcrumb()
+        testBreadcrumb.level = SentryLevel.INFO
+        testBreadcrumb.type = "navigation"
+        testBreadcrumb.category = "navigation"
+        testBreadcrumb.setData("to", "ProfileScreen")
+        val actual = converter.convert(testBreadcrumb) as RRWebBreadcrumbEvent
+
+        assertRRWebBreadcrumbDefaults(actual)
+        assertEquals(SentryLevel.INFO, actual.level)
+        assertEquals("navigation", actual.category)
+        assertEquals(null, actual.data?.get("from"))
+        assertEquals("ProfileScreen", actual.data?.get("to"))
+    }
+
+    @Test
+    fun convertForegroundBreadcrumb() {
         val converter = RNSentryReplayBreadcrumbConverter()
         val testBreadcrumb = Breadcrumb()
         testBreadcrumb.type = "navigation"
@@ -20,11 +57,12 @@ class RNSentryReplayBreadcrumbConverterTest {
         testBreadcrumb.setData("state", "foreground");
         val actual = converter.convert(testBreadcrumb) as RRWebBreadcrumbEvent
 
+        assertRRWebBreadcrumbDefaults(actual)
         assertEquals("app.foreground", actual.category)
     }
 
     @Test
-    fun testConvertBackgroundBreadcrumb() {
+    fun convertBackgroundBreadcrumb() {
         val converter = RNSentryReplayBreadcrumbConverter()
         val testBreadcrumb = Breadcrumb()
         testBreadcrumb.type = "navigation"
@@ -32,6 +70,7 @@ class RNSentryReplayBreadcrumbConverterTest {
         testBreadcrumb.setData("state", "background");
         val actual = converter.convert(testBreadcrumb) as RRWebBreadcrumbEvent
 
+        assertRRWebBreadcrumbDefaults(actual)
         assertEquals("app.background", actual.category)
     }
 
@@ -51,6 +90,32 @@ class RNSentryReplayBreadcrumbConverterTest {
         testBreadcrumb.category = "sentry.transaction"
         val actual = converter.convert(testBreadcrumb)
         assertEquals(null, actual)
+    }
+
+    @Test
+    fun convertTouchBreadcrumb() {
+        val converter = RNSentryReplayBreadcrumbConverter()
+        val testBreadcrumb = Breadcrumb()
+        testBreadcrumb.level = SentryLevel.INFO
+        testBreadcrumb.type = "user"
+        testBreadcrumb.category = "touch"
+        testBreadcrumb.message = "this won't be used for replay"
+        testBreadcrumb.setData(
+            "path",
+            arrayListOf(mapOf(
+                "element" to "element4",
+                "file" to "file4")))
+        val actual = converter.convert(testBreadcrumb) as RRWebBreadcrumbEvent
+
+        assertRRWebBreadcrumbDefaults(actual)
+        assertEquals(SentryLevel.INFO, actual.level)
+        assertEquals("ui.tap", actual.category)
+        assertEquals(1, actual.data?.keys?.size)
+        assertEquals(
+            arrayListOf(mapOf(
+                "element" to "element4",
+                "file" to "file4")),
+            actual.data?.get("path"))
     }
 
     @Test
@@ -95,5 +160,11 @@ class RNSentryReplayBreadcrumbConverterTest {
             mapOf("label" to "label6"),
             mapOf("name" to "name7")))
         assertEquals("label5(element5, file5) > label4(file4) > label3(element3) > label2", actual)
+    }
+
+    private fun assertRRWebBreadcrumbDefaults(actual: RRWebBreadcrumbEvent) {
+        assertEquals("default", actual.breadcrumbType)
+        assertEquals(actual.breadcrumbTimestamp * 1000, actual.timestamp.toDouble(), 0.05)
+        assert(actual.breadcrumbTimestamp > 0)
     }
 }
