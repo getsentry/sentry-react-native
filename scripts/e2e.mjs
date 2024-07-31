0442bfa5959938b15accc2f9fed0767f25c9a2ec
Ivan Dlugos <dlugos.ivan@gmail.com>
2024-07-31 23:24:25 +0200

    fixes

--- scripts/e2e.mjs
+++ scripts/e2e.mjs
@@ -64,6 +64,8 @@ const appName = 'RnDiffApp';
 const appDir = `${appRepoDir}/${appName}`;
 const e2eDir = `${rootDir}/test/e2e`;
 const testAppName = `${appName}.${platform == 'ios' ? 'app' : 'apk'}`;
+const runtime = env.IOS_RUNTIME ? env.IOS_RUNTIME : 'latest';
+const device = env.IOS_DEVICE ? env.IOS_DEVICE : 'iPhone 15';
 
 // Build and publish the SDK - we only need to do this once in CI.
 // Locally, we may want to get updates from the latest build so do it on every app build.
@@ -137,9 +139,6 @@ if (actions.includes('build')) {
   var appProduct;
 
   if (platform == 'ios') {
-    const runtime = env.IOS_RUNTIME ? env.IOS_RUNTIME : 'latest';
-    const device = env.IOS_DEVICE ? env.IOS_DEVICE : 'iPhone 15';
-
     // Build iOS test app
     execSync(`set -o pipefail && xcodebuild \
     -workspace ${appName}.xcworkspace \
