9afb6bc40a84e28746b90608892bc170140f2ae9
Krystof Woldrich <krystof.woldrich@sentry.io>
2024-08-05 09:59:57 +0200

    Add tests

--- test/client.test.ts
+++ test/client.test.ts
@@ -349,16 +349,6 @@ describe('Tests ReactNativeClient', () => {
 
     beforeEach(() => {
       mockTransportSend = jest.fn(() => Promise.resolve());
-      client = new ReactNativeClient({
-        ...DEFAULT_OPTIONS,
-        attachStacktrace: true,
-        stackParser: defaultStackParser,
-        dsn: EXAMPLE_DSN,
-        transport: () => ({
-          send: mockTransportSend,
-          flush: jest.fn(),
-        }),
-      } as ReactNativeClientOptions);
     });
 
     afterEach(() => {
@@ -369,11 +359,41 @@ describe('Tests ReactNativeClient', () => {
       func.mock.calls[0][firstArg][envelopeItems][0][envelopeItemPayload];
 
     test('captureMessage contains stack trace in exception', async () => {
+      client = new ReactNativeClient({
+        ...DEFAULT_OPTIONS,
+        attachStacktrace: true,
+        stackParser: defaultStackParser,
+        dsn: EXAMPLE_DSN,
+        transport: () => ({
+          send: mockTransportSend,
+          flush: jest.fn(),
+        }),
+      } as ReactNativeClientOptions);
+
       const mockSyntheticExceptionFromHub = new Error();
       client.captureMessage('test message', 'error', { syntheticException: mockSyntheticExceptionFromHub });
       expect(getMessageEventFrom(mockTransportSend).exception.values.length).toBeGreaterThan(0);
       expect(getMessageEventFrom(mockTransportSend).exception).toBeDefined();
       expect(getMessageEventFrom(mockTransportSend).threads).toBeUndefined();
+    });
+
+    test('captureMessage contains stack trace in exception', async () => {
+      client = new ReactNativeClient({
+        ...DEFAULT_OPTIONS,
+        attachStacktrace: true,
+        stackParser: defaultStackParser,
+        dsn: EXAMPLE_DSN,
+        transport: () => ({
+          send: mockTransportSend,
+          flush: jest.fn(),
+        }),
+        useThreadsForMessageStack: true,
+      } as ReactNativeClientOptions);
+
+      const mockSyntheticExceptionFromHub = new Error();
+      client.captureMessage('test message', 'error', { syntheticException: mockSyntheticExceptionFromHub });
+      expect(getMessageEventFrom(mockTransportSend).threads.values.length).toBeGreaterThan(0);
+      expect(getMessageEventFrom(mockTransportSend).exception).toBeUndefined();
     });
   });
 
