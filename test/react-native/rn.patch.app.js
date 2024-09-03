8bf63c7ed75d2b22fbd9d8d11c5d0dcb98ac8561
Ivan Dlugos <dlugos.ivan@gmail.com>
2024-09-03 19:43:03 +0200

    disable session replay in tests

--- test/react-native/rn.patch.app.js
+++ test/react-native/rn.patch.app.js
@@ -28,7 +28,6 @@ Sentry.init({
   dsn: 'https://1df17bd4e543fdb31351dee1768bb679@o447951.ingest.sentry.io/5428561',
   debug: true,
   _experiments: {
-    replaysSessionSampleRate: 1.0,
     replaysOnErrorSampleRate: 1.0,
   },
   integrations: [
