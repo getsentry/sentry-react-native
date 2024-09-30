d923fa01127dd899ac85e7f9a3434336f5c8f25b
Krystof Woldrich <krystof.woldrich@sentry.io>
2024-09-30 22:48:53 +0200

    misc: Move macos sample to a standalone project

---
+++ samples/react-native-macos/src/Screens/GesturesTracingScreen.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
+import { sentryTraceGesture, startSpanManual } from '@sentry/react-native';
+import { Span } from '@sentry/types';
+
+const GesturesTracingScreen = () => {
+  const gesture = Gesture.Pinch().onBegin(() => {
+    startExampleSpan();
+  });
+
+  return (
+    <GestureDetector gesture={sentryTraceGesture('pinch', gesture)}>
+      <View style={styles.screen}>
+        <Text>Do pinch gesture</Text>
+      </View>
+    </GestureDetector>
+  );
+};
+
+const startExampleSpan = () => {
+  startSpanManual({ name: 'Example', op: 'example' }, (span: Span) => {
+    setTimeout(() => {
+      span.end();
+    }, 1000);
+  });
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default GesturesTracingScreen;
