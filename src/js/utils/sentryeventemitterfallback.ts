ae79ef22c3896b3529c5778eee44bcfeee3df8b6
Krystof Woldrich <krystof.woldrich@sentry.io>
2024-10-14 15:32:35 +0200

    Emitter suggestion

--- src/js/utils/sentryeventemitterfallback.ts
+++ src/js/utils/sentryeventemitterfallback.ts
@@ -1,8 +1,8 @@
 import { logger, timestampInSeconds } from '@sentry/utils';
-import { DeviceEventEmitter } from 'react-native';
 
 import { NATIVE } from '../wrapper';
-import { NewFrameEventName } from './sentryeventemitter';
+import type { NewFrameEvent } from './sentryeventemitter';
+import { createSentryEventEmitter, NewFrameEventName } from './sentryeventemitter';
 
 export type FallBackNewFrameEvent = { newFrameTimestampInSeconds: number; isFallback?: boolean };
 export interface SentryEventEmitterFallback {
@@ -11,83 +11,83 @@ export interface SentryEventEmitterFallback {
    * This method is synchronous in JS but the event emitter starts asynchronously.
    */
   initAsync: () => void;
-  startListenerAsync: () => void;
+  onceNewFrame: (listener: (event: FallBackNewFrameEvent) => void) => void;
 }
 
 /**
  * Creates emitter that allows to listen to UI Frame events when ready.
  */
 export function createSentryFallbackEventEmitter(): SentryEventEmitterFallback {
-  let NativeEmitterCalled: boolean = false;
-  let isListening = false;
+  const emitter = createSentryEventEmitter();
 
-  function defaultFallbackEventEmitter(): void {
+  let fallbackTimeout: ReturnType<typeof setTimeout> | undefined;
+  let animationFrameTimestampSeconds: number | undefined;
+  let nativeNewFrameTimestampSeconds: number | undefined;
+
+  function getAnimationFrameTimestampSeconds(): void {
     // https://reactnative.dev/docs/timers#timers
     // NOTE: The current implementation of requestAnimationFrame is the same
     // as setTimeout(0). This isn't exactly how requestAnimationFrame
     // is supposed to work on web, so it doesn't get called when UI Frames are rendered.: https://github.com/facebook/react-native/blob/5106933c750fee2ce49fe1945c3e3763eebc92bc/packages/react-native/ReactCommon/react/runtime/TimerManager.cpp#L442-L443
     requestAnimationFrame(() => {
-      if (NativeEmitterCalled) {
-        NativeEmitterCalled = false;
-        isListening = false;
+      if (fallbackTimeout === undefined) {
         return;
       }
-      const seconds = timestampInSeconds();
-      waitForNativeResponseOrFallback(seconds, 'JavaScript');
+      animationFrameTimestampSeconds = timestampInSeconds();
     });
   }
 
-  function waitForNativeResponseOrFallback(fallbackSeconds: number, origin: string): void {
-    let firstAttemptCompleted = false;
-
-    const checkNativeResponse = (): void => {
-      if (NativeEmitterCalled) {
-        NativeEmitterCalled = false;
-        isListening = false;
-        return; // Native Replied the bridge with a timestamp.
-      }
-      if (!firstAttemptCompleted) {
-        firstAttemptCompleted = true;
-        setTimeout(checkNativeResponse, 3_000);
-      } else {
-        logger.log(`[Sentry] Native event emitter did not reply in time. Using ${origin} fallback emitter.`);
-        isListening = false;
-        DeviceEventEmitter.emit(NewFrameEventName, {
-          newFrameTimestampInSeconds: fallbackSeconds,
-          isFallback: true,
-        });
-      }
-    };
-
-    // Start the retry process
-    checkNativeResponse();
+  function getNativeNewFrameTimestampSeconds(): void {
+    NATIVE.getNewScreenTimeToDisplay()
+      .then(resolve => {
+        if (fallbackTimeout === undefined) {
+          return;
+        }
+        nativeNewFrameTimestampSeconds = resolve ?? undefined;
+      })
+      .catch(reason => {
+        logger.error('Failed to receive Native fallback timestamp.', reason);
+      });
   }
 
   return {
     initAsync() {
-      DeviceEventEmitter.addListener(NewFrameEventName, () => {
-        // Avoid noise from pages that we do not want to track.
-        if (isListening) {
-          NativeEmitterCalled = true;
-        }
-      });
+      emitter.initAsync(NewFrameEventName);
     },
 
-    startListenerAsync() {
-      isListening = true;
+    onceNewFrame(listener: (event: FallBackNewFrameEvent) => void) {
+      animationFrameTimestampSeconds = undefined;
+      nativeNewFrameTimestampSeconds = undefined;
 
-      NATIVE.getNewScreenTimeToDisplay()
-        .then(resolve => {
-          if (resolve) {
-            waitForNativeResponseOrFallback(resolve, 'Native');
-          } else {
-            defaultFallbackEventEmitter();
-          }
-        })
-        .catch((reason: Error) => {
-          logger.error('Failed to recceive Native fallback timestamp.', reason);
-          defaultFallbackEventEmitter();
-        });
+      const internalListener = (event: NewFrameEvent): void => {
+        clearTimeout(fallbackTimeout);
+        fallbackTimeout = undefined;
+        animationFrameTimestampSeconds = undefined;
+        nativeNewFrameTimestampSeconds = undefined;
+        listener(event);
+      };
+      fallbackTimeout = setTimeout(() => {
+        if (nativeNewFrameTimestampSeconds) {
+          logger.log('Native fallback timestamp received.');
+          return listener({
+            newFrameTimestampInSeconds: nativeNewFrameTimestampSeconds,
+            isFallback: true,
+          });
+        } else if (animationFrameTimestampSeconds) {
+          logger.log('[Sentry] Native event emitter did not reply in time. Using JavaScript fallback emitter.');
+          return listener({
+            newFrameTimestampInSeconds: animationFrameTimestampSeconds,
+            isFallback: true,
+          });
+        } else {
+          emitter.removeListener(NewFrameEventName, internalListener);
+          logger.error('Failed to receive any fallback timestamp.');
+        }
+      }, 3_000);
+
+      getNativeNewFrameTimestampSeconds();
+      getAnimationFrameTimestampSeconds();
+      emitter.once(NewFrameEventName, internalListener);
     },
   };
 }
