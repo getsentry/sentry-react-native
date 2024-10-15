bc321543ea55cb00cb9075a9ee6ad3b6f73fd010
Antonis Lilis <antonis.lilis@gmail.com>
2024-10-15 11:49:41 +0300

    Adds babel/plugin-transform-private-methods to fix broken tests

--- packages/core/babel.config.js
+++ packages/core/babel.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    ['@babel/plugin-transform-class-properties', { loose: true }],
+    ['@babel/plugin-transform-private-methods', { loose: true }],
+    ['@babel/plugin-transform-private-property-in-object', { loose: true }]
+  ],
 };
+
