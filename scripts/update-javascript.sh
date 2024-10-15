6625020e90345a14c5811503364fbd4d7a039dd3
Krystof Woldrich <krystof.woldrich@sentry.io>
2024-10-15 14:16:49 +0200

    move corepack to common script for js updates

--- scripts/update-javascript.sh
+++ scripts/update-javascript.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-corepack enable # This repository uses Yarn v3 which requires corepack to be enabled
-
 tagPrefix=''
 repo="https://github.com/getsentry/sentry-javascript.git"
 packages=('@sentry/browser' '@sentry/core' '@sentry/react' '@sentry/types' '@sentry/utils' '@sentry-internal/typescript')
