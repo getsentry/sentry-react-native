2a7c8faadf298643d89dfa2b8b9d9fdcf30247f5
lucas <lucas-zimerman1@hotmail.com>
2024-10-15 13:13:54 -0300

    always use main thread on ttid

--- android/src/main/java/io/sentry/react/RNSentryTimeToDisplay.java
+++ android/src/main/java/io/sentry/react/RNSentryTimeToDisplay.java
@@ -2,6 +2,8 @@ package io.sentry.react;
 
 import com.facebook.react.bridge.Promise;
 
+import android.os.Handler;
+import android.os.Looper;
 import android.view.Choreographer;
 
 import org.jetbrains.annotations.NotNull;
@@ -11,18 +13,27 @@ import io.sentry.android.core.SentryAndroidDateProvider;
 
 public class RNSentryTimeToDisplay {
     public static void GetTimeToDisplay(Promise promise, SentryDateProvider dateProvider) {
-        try {
-            Choreographer choreographer = Choreographer.getInstance();
+        Looper mainLooper = Looper.getMainLooper();
 
-            // Invoke the callback after the frame is rendered
-            choreographer.postFrameCallback(frameTimeNanos -> {
-                final SentryDate endDate = dateProvider.now();
+        if (mainLooper == null) {
+            promise.reject("GetTimeToDisplay is not able to measure the time to display: Main looper not available.");
+            return;
+        }
+        
+        // Ensure the code runs on the main thread
+        new Handler(mainLooper)
+            .post(() -> {
+                try {
+                    Choreographer choreographer = Choreographer.getInstance();
 
-                promise.resolve(endDate.nanoTimestamp() / 1e9);
+                    // Invoke the callback after the frame is rendered
+                    choreographer.postFrameCallback(frameTimeNanos -> {
+                        final SentryDate endDate = dateProvider.now();
+                        promise.resolve(endDate.nanoTimestamp() / 1e9);
+                    });
+                } catch (Exception exception) {
+                    promise.reject("Failed to receive the instance of Choreographer", exception);
+                }
             });
-        }
-        catch (Exception exception) {
-            promise.reject("Failed to receive the instance of Choreographer" ,exception);
-        }
     }
 }
