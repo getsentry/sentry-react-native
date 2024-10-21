3b730cbe0cd8c0a1473cf9298e321910a3b944f0
Antonis Lilis <antonis.lilis@gmail.com>
2024-10-21 14:03:52 +0300

    Adds @react-native/babel-preset

--- packages/core/babel.config.js
+++ packages/core/babel.config.js
@@ -1,9 +1,4 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    ['@babel/plugin-transform-class-properties', { loose: true }],
-    ['@babel/plugin-transform-private-methods', { loose: true }],
-    ['@babel/plugin-transform-private-property-in-object', { loose: true }]
-  ],
+  presets: ['module:@react-native/babel-preset'],
 };
 
