feedb68a376f42ed60421985d7c9deace7b04527
Krystof Woldrich <krystof.woldrich@sentry.io>
2024-11-13 16:43:47 +0100

    Add implementation explanation

--- packages/core/src/js/replay/CustomMask.web.tsx
+++ packages/core/src/js/replay/CustomMask.web.tsx
@@ -1,6 +1,17 @@
 import * as React from 'react';
 import { View, ViewProps } from 'react-native';
 
+// Wrapping children in a View and div can cause styling issues
+// but with the current implementation of react-native-web
+// we can't avoid it.
+//
+// <View unknown-prop /> the prop is dropped by react-native-web
+// https://github.com/necolas/react-native-web/blob/a5ba27c6226aa182979a9cff8cc23c0f5caa4d88/packages/react-native-web/src/exports/View/index.js#L47
+//
+// So we need to wrap the children in a react-dom div.
+// We are using className instead of data-attribute to
+// allow for easier CSS styling adjustments.
+
 const Mask = (props: ViewProps): React.ReactElement => {
   return (
     <View {...props}>
