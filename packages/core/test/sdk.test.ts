b6191bead407627af0992f346bad63707f5995ba
Antonis Lilis <antonis.lilis@gmail.com>
2024-11-18 15:31:42 +0200

    Adds test case for url exception

--- packages/core/test/sdk.test.ts
+++ packages/core/test/sdk.test.ts
@@ -353,6 +353,39 @@ describe('Tests the SDK functionality', () => {
       expect(result).toBeNull();
     });
 
+    it('should keep breadcrumbs matching dsn if the url parsing fails for dsn', () => {
+      (getDevServer as jest.Mock).mockReturnValue({ url: 'http://localhost:8081' });
+
+      const mockBeforeBreadcrumb = (breadcrumb: Breadcrumb, _hint?: BreadcrumbHint) => {
+        return breadcrumb;
+      };
+
+      // Mock the URL constructor to throw an exception for this test case
+      const originalURL = (global as any).URL;
+      jest.spyOn(global as any, 'URL').mockImplementationOnce(() => {
+        throw new Error('Failed to parse DSN URL');
+      });
+
+      const passedOptions = {
+        dsn: 'https://abc@def.ingest.sentry.io/1234567',
+        beforeBreadcrumb: mockBeforeBreadcrumb,
+      };
+
+      init(passedOptions);
+
+      const breadcrumb: Breadcrumb = {
+        type: 'http',
+        data: { url: 'https://def.ingest.sentry.io/1234567' },
+      };
+
+      const result = usedOptions()?.beforeBreadcrumb!(breadcrumb);
+
+      expect(result).toEqual(breadcrumb);
+
+      // Restore the original URL constructor
+      (global as any).URL = originalURL;
+    });
+
     it('should keep non dev server or dsn breadcrumbs', () => {
       (getDevServer as jest.Mock).mockReturnValue({ url: 'http://localhost:8081' });
 
