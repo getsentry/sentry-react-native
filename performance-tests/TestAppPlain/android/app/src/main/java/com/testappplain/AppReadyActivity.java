8bdc153fe1d205b6b685dc87e0ac2bfd57d82259
Antonis Lilis <antonis.lilis@gmail.com>
2024-11-19 18:28:21 +0200

    Remove unneeded comments

--- performance-tests/TestAppPlain/android/app/src/main/java/com/testappplain/AppReadyActivity.java
+++ performance-tests/TestAppPlain/android/app/src/main/java/com/testappplain/AppReadyActivity.java
@@ -12,21 +12,15 @@ public class AppReadyActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
-    // Create a LinearLayout to hold the TextView
+    // Add a simple text saying that the app is fully loaded
     LinearLayout layout = new LinearLayout(this);
     layout.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
     layout.setGravity(Gravity.CENTER);
-
-    // Create a TextView with the text "App Ready"
     TextView textView = new TextView(this);
     textView.setText("The App is fully loaded!\nTap/swipe back to close this view.");
-    textView.setTextSize(24); // Set text size
+    textView.setTextSize(24);
     textView.setGravity(Gravity.CENTER);
-
-    // Add the TextView to the LinearLayout
     layout.addView(textView);
-
-    // Set the LinearLayout as the content view
     setContentView(layout);
   }
 }
