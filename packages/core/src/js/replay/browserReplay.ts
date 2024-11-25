147d1752f39e77e140ff199e9d7843bacb854afa
Krystof Woldrich <krystof.woldrich@sentry.io>
2024-11-25 15:11:40 +0100

    remove unnecessary div selector

--- packages/core/src/js/replay/browserReplay.ts
+++ packages/core/src/js/replay/browserReplay.ts
@@ -3,8 +3,8 @@ import { replayIntegration } from '@sentry/react';
 const browserReplayIntegration = (options: Parameters<typeof replayIntegration>[0] = {}) => {
   return replayIntegration({
     ...options,
-    mask: ['div.sentry-react-native-mask', ...(options.mask || [])],
-    unmask: ['div.sentry-react-native-unmask:not(div.sentry-react-native-mask *) > *', ...(options.unmask || [])],
+    mask: ['.sentry-react-native-mask', ...(options.mask || [])],
+    unmask: ['.sentry-react-native-unmask:not(.sentry-react-native-mask *) > *', ...(options.unmask || [])],
   });
 };
 
