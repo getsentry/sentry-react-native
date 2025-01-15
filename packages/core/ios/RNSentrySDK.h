890133a86bf4f71cc6dce056b8aecde26feaa00f
Krystof Woldrich <krystof.woldrich@sentry.io>
2025-01-15 14:47:32 +0100

    Update code docs for start methods

--- packages/core/ios/RNSentrySDK.h
+++ packages/core/ios/RNSentrySDK.h
@@ -5,8 +5,8 @@ SENTRY_NO_INIT
 
 /**
  * @experimental
- * Inits and configures Sentry for React Native applications. Make sure to
- * set a valid DSN.
+ * Inits and configures Sentry for React Native applications using `sentry.options.json`
+ * configuration file.
  *
  * @discussion Call this method on the main thread. When calling it from a background thread, the
  * SDK starts on the main thread async.
@@ -15,8 +15,11 @@ SENTRY_NO_INIT
 
 /**
  * @experimental
- * Inits and configures Sentry for React Native applications. Make sure to
- * set a valid DSN.
+ * Inits and configures Sentry for React Native applicationsusing `sentry.options.json`
+ * configuration file and `configureOptions` callback.
+ *
+ * The `configureOptions` callback can overwrite the config file options
+ * and add non-serializable items to the options object.
  *
  * @discussion Call this method on the main thread. When calling it from a background thread, the
  * SDK starts on the main thread async.
