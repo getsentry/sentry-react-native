c25996e4512328c4a2ffbcdcf8f740d3861a963a
Antonis Lilis <antonis.lilis@gmail.com>
2025-01-16 12:57:47 +0200

    Use a library to get the Uint8Array

--- samples/react-native/src/App.tsx
+++ samples/react-native/src/App.tsx
@@ -37,7 +37,7 @@ import { ErrorEvent } from '@sentry/core';
 import HeavyNavigationScreen from './Screens/HeavyNavigationScreen';
 import WebviewScreen from './Screens/WebviewScreen';
 import { isTurboModuleEnabled } from '@sentry/react-native/dist/js/utils/environment';
-
+import { toByteArray } from 'react-native-quick-base64';
 import { launchImageLibrary } from 'react-native-image-picker';
 
 if (typeof setImmediate === 'undefined') {
@@ -141,36 +141,6 @@ const Stack = isMobileOs
   : createStackNavigator();
 const Tab = createBottomTabNavigator();
 
-/* eslint-disable no-bitwise */
-const base64ToUint8Array = (base64?: string): Uint8Array | undefined => {
-  if (!base64) return undefined;
-
-  const cleanedBase64 = base64.replace(/^data:.*;base64,/, ''); // Remove any prefix before the base64 string
-  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
-  const bytes: number[] = [];
-
-  let buffer = 0;
-  let bits = 0;
-
-  for (const char of cleanedBase64) {
-    if (char === '=') break;
-
-    const value = chars.indexOf(char); // Validate each character
-    if (value === -1) return undefined;
-
-    buffer = (buffer << 6) | value; // Shift 6 bits to the left and add the value
-    bits += 6;
-
-    if (bits >= 8) {
-      // Add a byte when we have 8 or more bits
-      bits -= 8;
-      bytes.push((buffer >> bits) & 0xff);
-    }
-  }
-
-  return new Uint8Array(bytes);
-};
-
 const handleChooseImage = (attachFile: (filename: string, data: Uint8Array) => void): void => {
   launchImageLibrary({ mediaType: 'photo', includeBase64: true }, (response) => {
     if (response.didCancel) {
@@ -180,7 +150,7 @@ const handleChooseImage = (attachFile: (filename: string, data: Uint8Array) => v
     } else if (response.assets && response.assets.length > 0) {
       const filename = response.assets[0].fileName;
       const base64String = response.assets[0].base64;
-      const screenShotUint8Array = base64ToUint8Array(base64String);
+      const screenShotUint8Array = toByteArray(base64String);
       if (filename && screenShotUint8Array) {
         attachFile(filename, screenShotUint8Array);
       }
