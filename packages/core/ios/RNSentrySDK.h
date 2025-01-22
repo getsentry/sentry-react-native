78506779fe155a29fb893c1cd10a792b30b41928
Krystof Woldrich <31292499+krystofwoldrich@users.noreply.github.com>
2025-01-22 17:55:44 +0100

    feat: Read `sentry.options.json` during cocoa init (#4447)

--- packages/core/ios/RNSentrySDK.h
+++ packages/core/ios/RNSentrySDK.h
@@ -5,8 +5,21 @@ SENTRY_NO_INIT
 
 /**
  * @experimental
- * Inits and configures Sentry for React Native applications. Make sure to
- * set a valid DSN.
+ * Inits and configures Sentry for React Native applications using `sentry.options.json`
+ * configuration file.
+ *
+ * @discussion Call this method on the main thread. When calling it from a background thread, the
+ * SDK starts on the main thread async.
+ */
++ (void)start;
+
+/**
+ * @experimental
+ * Inits and configures Sentry for React Native applicationsusing `sentry.options.json`
+ * configuration file and `configureOptions` callback.
+ *
+ * The `configureOptions` callback can overwrite the config file options
+ * and add non-serializable items to the options object.
  *
  * @discussion Call this method on the main thread. When calling it from a background thread, the
  * SDK starts on the main thread async.
--- samples/react-native/ios/sentryreactnativesample/AppDelegate.mm
+++ samples/react-native/ios/sentryreactnativesample/AppDelegate.mm
@@ -20,49 +20,12 @@ AppDelegate () <RCTTurboModuleManagerDelegate> {
 
 @implementation AppDelegate
 
-- (void)initializeSentry
-{
-    [SentrySDK startWithConfigureOptions:^(SentryOptions *options) {
-        // Only options set here will apply to the iOS SDK
-        // Options from JS are not passed to the iOS SDK when initialized manually
-        options.dsn = @"https://1df17bd4e543fdb31351dee1768bb679@o447951.ingest.sentry.io/5428561";
-        options.debug = YES; // Enabled debug when first installing is always helpful
-
-        options.beforeSend = ^SentryEvent *(SentryEvent *event)
-        {
-            // We don't want to send an event after startup that came from a Unhandled JS Exception
-            // of react native Because we sent it already before the app crashed.
-            if (nil != event.exceptions.firstObject.type &&
-                [event.exceptions.firstObject.type rangeOfString:@"Unhandled JS Exception"].location
-                    != NSNotFound) {
-                NSLog(@"Unhandled JS Exception");
-                return nil;
-            }
-
-            return event;
-        };
-
-        // Enable the App start and Frames tracking measurements
-        // If this is disabled the app start and frames tracking
-        // won't be passed from native to JS transactions
-        PrivateSentrySDKOnly.appStartMeasurementHybridSDKMode = true;
-#if TARGET_OS_IPHONE || TARGET_OS_MACCATALYST
-        PrivateSentrySDKOnly.framesTrackingMeasurementHybridSDKMode = true;
-#endif
-    }];
-}
-
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // When the native init is enabled the `autoInitializeNativeSdk`
     // in JS has to be set to `false`
-    // [self initializeSentry];
-    //    [RNSentrySDK startWithConfigureOptions:^(SentryOptions *options) {
-    //        options.dsn =
-    //        @"https://1df17bd4e543fdb31351dee1768bb679@o447951.ingest.sentry.io/5428561";
-    //        options.debug = YES;
-    //    }];
+    // [RNSentrySDK start];
 
     self.moduleName = @"sentry-react-native-sample";
     // You can add your custom initial props in the dictionary below.
