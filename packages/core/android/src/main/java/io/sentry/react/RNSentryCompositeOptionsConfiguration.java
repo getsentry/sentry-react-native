b6a36a1954d471f8d1942f7dc498ed9482aa07a0
Antonis Lilis <antonis.lilis@gmail.com>
2025-01-24 12:17:45 +0200

    Allow combining more than two configurations

--- packages/core/android/src/main/java/io/sentry/react/RNSentryCompositeOptionsConfiguration.java
+++ packages/core/android/src/main/java/io/sentry/react/RNSentryCompositeOptionsConfiguration.java
@@ -2,22 +2,24 @@ package io.sentry.react;
 
 import io.sentry.Sentry.OptionsConfiguration;
 import io.sentry.android.core.SentryAndroidOptions;
+import java.util.List;
 import org.jetbrains.annotations.NotNull;
 
 class RNSentryCompositeOptionsConfiguration implements OptionsConfiguration<SentryAndroidOptions> {
-  private final OptionsConfiguration<SentryAndroidOptions> baseConfiguration;
-  private final OptionsConfiguration<SentryAndroidOptions> overridingConfiguration;
+  private final @NotNull List<OptionsConfiguration<SentryAndroidOptions>> configurations;
 
-  RNSentryCompositeOptionsConfiguration(
-      OptionsConfiguration<SentryAndroidOptions> baseConfiguration,
-      OptionsConfiguration<SentryAndroidOptions> overridingConfiguration) {
-    this.baseConfiguration = baseConfiguration;
-    this.overridingConfiguration = overridingConfiguration;
+  @SafeVarargs
+  protected RNSentryCompositeOptionsConfiguration(
+      @NotNull OptionsConfiguration<SentryAndroidOptions>... configurations) {
+    this.configurations = List.of(configurations);
   }
 
   @Override
   public void configure(@NotNull SentryAndroidOptions options) {
-    baseConfiguration.configure(options);
-    overridingConfiguration.configure(options);
+    for (OptionsConfiguration<SentryAndroidOptions> configuration : configurations) {
+      if (configuration != null) {
+        configuration.configure(options);
+      }
+    }
   }
 }
