4341263bffacaac774137a855f81427fd676ab28
Antonis Lilis <antonis.lilis@gmail.com>
2025-02-07 13:19:27 +0200

    Lint fixes

--- performance-tests/TestAppSentry/ios/TestAppSentry/AppDelegate.swift
+++ performance-tests/TestAppSentry/ios/TestAppSentry/AppDelegate.swift
@@ -1,11 +1,11 @@
-import UIKit
 import React
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
+import UIKit
 
 @main
 class AppDelegate: RCTAppDelegate {
-  override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+  override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
     self.moduleName = "TestAppSentry"
     self.dependencyProvider = RCTAppDependencyProvider()
 
