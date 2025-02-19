4c62cf6766bb7ce9b447137562ba7c84c6e3a527
Krystof Woldrich <31292499+krystofwoldrich@users.noreply.github.com>
2025-02-19 16:51:20 +0100

    Update collect-modules.sh

--- packages/core/scripts/collect-modules.sh
+++ packages/core/scripts/collect-modules.sh
@@ -45,7 +45,7 @@ else
 fi
 
 type $nodePath >/dev/null 2>&1 || {
-  echo >&2 "[sentry/collect-modules.sh] $nodePath not found! Modules won't be collected." \
+  echo "[sentry/collect-modules.sh] $nodePath not found! Modules won't be collected." \
     "Please export NODE_BINARY in 'Build Phase' - 'Bundle React Native code and images'" \
     "to an absolute path of your node binary. Check your node path by 'which node'."
   exit 0 # Don't fail the build but inform about the problem
