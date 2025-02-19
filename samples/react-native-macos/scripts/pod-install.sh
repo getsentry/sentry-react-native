f2a14c95011342fdaa042ad6c1a1f30b97c2de29
Krystof Woldrich <krystof.woldrich@sentry.io>
2025-02-19 09:20:17 +0100

    fix ci

--- samples/react-native-macos/scripts/pod-install.sh
+++ samples/react-native-macos/scripts/pod-install.sh
@@ -12,7 +12,7 @@ echo "ENABLE_NEW_ARCH=$ENABLE_NEW_ARCH"
 cd "${thisFilePath}/.."
 bundle install
 
-cd ios
+cd macos
 PRODUCTION=$ENABLE_PROD RCT_NEW_ARCH_ENABLED=$ENABLE_NEW_ARCH bundle exec pod update
 
 cat Podfile.lock | grep $RN_SENTRY_POD_NAME
