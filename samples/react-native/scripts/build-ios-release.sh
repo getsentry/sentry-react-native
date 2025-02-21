e935360ae40e6445f13a3c8a0150a739fd27112e
Krystof Woldrich <31292499+krystofwoldrich@users.noreply.github.com>
2025-02-21 08:29:27 +0100

    chore(samples): Add package scripts for native builds, dsn and testing (#4561)

---
+++ samples/react-native/scripts/build-ios-release.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Exit on error and print commands
+set -xe
+
+thisFilePath=$(dirname "$0")
+
+export CONFIG='Release'
+
+"${thisFilePath}/build-ios.sh"
--- samples/react-native/scripts/set-ios-dsn.mjs
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-
-import { setIosDsn } from './dsn.mjs';
-
-setIosDsn();
