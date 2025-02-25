ae342a3a07de4a5e94f2658b5e3d1a4b8792967a
Krystof Woldrich <31292499+krystofwoldrich@users.noreply.github.com>
2025-02-25 12:46:16 +0100

    test(e2e): Verify captured Errors Screen transaction (#4584)

--- samples/react-native/e2e/captureMessage.test.android.ts
+++ samples/react-native/e2e/captureMessage.test.android.ts
@@ -1,12 +1,12 @@
 import { describe, it, beforeAll, expect, afterAll } from '@jest/globals';
-import { Envelope } from '@sentry/core';
+import { Envelope, EventItem } from '@sentry/core';
 import { device } from 'detox';
 import {
   createSentryServer,
   containingEvent,
 } from './utils/mockedSentryServer';
-import { HEADER, ITEMS } from './utils/consts';
 import { tap } from './utils/tap';
+import { getItemOfTypeFrom } from './utils/event';
 
 describe('Capture message', () => {
   let sentryServer = createSentryServer();
@@ -27,9 +27,7 @@ describe('Capture message', () => {
   });
 
   it('envelope contains message event', async () => {
-    const item = (envelope[ITEMS] as [{ type?: string }, unknown][]).find(
-      i => i[HEADER].type === 'event',
-    );
+    const item = getItemOfTypeFrom<EventItem>(envelope, 'event');
 
     expect(item).toEqual([
       {
@@ -45,5 +43,104 @@ describe('Capture message', () => {
         platform: 'javascript',
       }),
     ]);
+  });
+
+  it('contains device context', async () => {
+    const item = getItemOfTypeFrom<EventItem>(envelope, 'event');
+
+    expect(item?.[1]).toEqual(
+      expect.objectContaining({
+        contexts: expect.objectContaining({
+          device: expect.objectContaining({
+            battery_level: expect.any(Number),
+            battery_temperature: expect.any(Number),
+            boot_time: expect.any(String),
+            brand: expect.any(String),
+            charging: expect.any(Boolean),
+            connection_type: expect.any(String),
+            family: expect.any(String),
+            free_memory: expect.any(Number),
+            free_storage: expect.any(Number),
+            id: expect.any(String),
+            language: expect.any(String),
+            locale: expect.any(String),
+            low_memory: expect.any(Boolean),
+            manufacturer: expect.any(String),
+            memory_size: expect.any(Number),
+            model: expect.any(String),
+            model_id: expect.any(String),
+            online: expect.any(Boolean),
+            orientation: expect.any(String),
+            processor_count: expect.any(Number),
+            processor_frequency: expect.any(Number),
+            screen_density: expect.any(Number),
+            screen_dpi: expect.any(Number),
+            screen_height_pixels: expect.any(Number),
+            screen_width_pixels: expect.any(Number),
+            simulator: expect.any(Boolean),
+            storage_size: expect.any(Number),
+            timezone: expect.any(String),
+          }),
+        }),
+      }),
+    );
+  });
+
+  it('contains app context', async () => {
+    const item = getItemOfTypeFrom<EventItem>(envelope, 'event');
+
+    expect(item?.[1]).toEqual(
+      expect.objectContaining({
+        contexts: expect.objectContaining({
+          app: expect.objectContaining({
+            app_build: expect.any(String),
+            app_identifier: expect.any(String),
+            app_name: expect.any(String),
+            app_start_time: expect.any(String),
+            app_version: expect.any(String),
+            in_foreground: expect.any(Boolean),
+            view_names: ['ErrorsScreen'],
+          }),
+        }),
+      }),
+    );
+  });
+
+  it('contains os context', async () => {
+    const item = getItemOfTypeFrom<EventItem>(envelope, 'event');
+
+    expect(item?.[1]).toEqual(
+      expect.objectContaining({
+        contexts: expect.objectContaining({
+          os: {
+            build: expect.any(String),
+            kernel_version: expect.any(String),
+            name: 'Android',
+            rooted: expect.any(Boolean),
+            version: expect.any(String),
+          },
+        }),
+      }),
+    );
+  });
+
+  it('contains react native context', async () => {
+    const item = getItemOfTypeFrom<EventItem>(envelope, 'event');
+
+    expect(item?.[1]).toEqual(
+      expect.objectContaining({
+        contexts: expect.objectContaining({
+          react_native_context: {
+            expo: false,
+            fabric: expect.any(Boolean),
+            hermes_debug_info: expect.any(Boolean),
+            hermes_version: expect.any(String),
+            js_engine: 'hermes',
+            react_native_version: expect.any(String),
+            turbo_module: expect.any(Boolean),
+          },
+        }),
+      }),
+    );
   });
 });
--- samples/react-native/e2e/captureMessage.test.ios.ts
+++ samples/react-native/e2e/captureMessage.test.ios.ts
@@ -1,12 +1,12 @@
 import { describe, it, beforeAll, expect, afterAll } from '@jest/globals';
-import { Envelope } from '@sentry/core';
+import { Envelope, EventItem } from '@sentry/core';
 import { device } from 'detox';
 import {
   createSentryServer,
   containingEvent,
 } from './utils/mockedSentryServer';
-import { HEADER, ITEMS } from './utils/consts';
 import { tap } from './utils/tap';
+import { getItemOfTypeFrom } from './utils/event';
 
 describe('Capture message', () => {
   let sentryServer = createSentryServer();
@@ -27,9 +27,7 @@ describe('Capture message', () => {
   });
 
   it('envelope contains message event', async () => {
-    const item = (envelope[ITEMS] as [{ type?: string }, unknown][]).find(
-      i => i[HEADER].type === 'event',
-    );
+    const item = getItemOfTypeFrom<EventItem>(envelope, 'event');
 
     expect(item).toEqual([
       {
@@ -42,5 +40,87 @@ describe('Capture message', () => {
         platform: 'javascript',
       }),
     ]);
+  });
+
+  it('contains device context', async () => {
+    const item = getItemOfTypeFrom<EventItem>(envelope, 'event');
+
+    expect(item?.[1]).toEqual(
+      expect.objectContaining({
+        contexts: expect.objectContaining({
+          device: expect.objectContaining({
+            arch: expect.any(String),
+            family: expect.any(String),
+            free_memory: expect.any(Number),
+            locale: expect.any(String),
+            memory_size: expect.any(Number),
+            model: expect.any(String),
+            model_id: expect.any(String),
+            processor_count: expect.any(Number),
+            simulator: expect.any(Boolean),
+            thermal_state: expect.any(String),
+            usable_memory: expect.any(Number),
+          }),
+        }),
+      }),
+    );
+  });
+
+  it('contains app context', async () => {
+    const item = getItemOfTypeFrom<EventItem>(envelope, 'event');
+
+    expect(item?.[1]).toEqual(
+      expect.objectContaining({
+        contexts: expect.objectContaining({
+          app: expect.objectContaining({
+            app_build: expect.any(String),
+            app_identifier: expect.any(String),
+            app_name: expect.any(String),
+            app_start_time: expect.any(String),
+            app_version: expect.any(String),
+            in_foreground: expect.any(Boolean),
+            // view_names: ['ErrorsScreen-jn5qquvH9Nz'], // TODO: fix this generated hash should not be part of the name
+          }),
+        }),
+      }),
+    );
+  });
+
+  it('contains os context', async () => {
+    const item = getItemOfTypeFrom<EventItem>(envelope, 'event');
+
+    expect(item?.[1]).toEqual(
+      expect.objectContaining({
+        contexts: expect.objectContaining({
+          os: {
+            build: expect.any(String),
+            kernel_version: expect.any(String),
+            name: 'iOS',
+            rooted: expect.any(Boolean),
+            version: expect.any(String),
+          },
+        }),
+      }),
+    );
+  });
+
+  it('contains react native context', async () => {
+    const item = getItemOfTypeFrom<EventItem>(envelope, 'event');
+
+    expect(item?.[1]).toEqual(
+      expect.objectContaining({
+        contexts: expect.objectContaining({
+          react_native_context: {
+            expo: false,
+            fabric: expect.any(Boolean),
+            hermes_debug_info: expect.any(Boolean),
+            hermes_version: expect.any(String),
+            js_engine: 'hermes',
+            react_native_version: expect.any(String),
+            turbo_module: expect.any(Boolean),
+          },
+        }),
+      }),
+    );
   });
 });
