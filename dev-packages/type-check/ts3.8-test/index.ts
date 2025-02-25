39a81c751ff89ef0b34d0cb4ccc71b351fc1034e
LucasZF <lucas-zimerman1@hotmail.com>
2025-02-25 10:08:11 -0300

    fix check type (#4587)

--- dev-packages/type-check/ts3.8-test/index.ts
+++ dev-packages/type-check/ts3.8-test/index.ts
@@ -3,6 +3,8 @@ declare global {
   interface IDBObjectStore {}
   interface Window {
     fetch: any;
+    setTimeout: any;
+    document: any;
   }
   interface ShadowRoot {}
   interface BufferSource {}
@@ -19,6 +21,7 @@ declare global {
     redirectCount: number;
   }
   interface PerformanceEntry {}
+  interface Performance {}
 }
 import 'react-native';
 
