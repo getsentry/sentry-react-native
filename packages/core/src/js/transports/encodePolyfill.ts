a64450dd4471779feb124ee0e9c68655464abaaa
lucas <lucas-zimerman1@hotmail.com>
2025-04-07 16:40:48 -0300

    fix yarn lint

--- packages/core/src/js/transports/encodePolyfill.ts
+++ packages/core/src/js/transports/encodePolyfill.ts
@@ -1,14 +1,13 @@
 import { getMainCarrier, SDK_VERSION } from '@sentry/core';
 
-import { RN_GLOBAL_OBJ } from '../utils/worldwide';
+import type { RN_GLOBAL_OBJ } from '../utils/worldwide';
 import { utf8ToBytes } from '../vendor';
 
 export const useEncodePolyfill = (): void => {
   const globalCarriers = getMainCarrier().__SENTRY__;
 
-
   if (!globalCarriers) {
-    (globalCarriers  as Partial<(typeof RN_GLOBAL_OBJ)['__SENTRY__']>) = {};
+    (globalCarriers as Partial<(typeof RN_GLOBAL_OBJ)['__SENTRY__']>) = {};
   }
 
   globalCarriers[SDK_VERSION].encodePolyfill = encodePolyfill;
