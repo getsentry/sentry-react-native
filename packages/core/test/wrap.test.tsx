25ff7b71f3454d65635c22cdd3771fc50dbcc6f0
Antonis Lilis <antonis.lilis@gmail.com>
2025-04-14 15:10:00 +0300

    Test undefined ppRegistryIntegration

--- packages/core/test/wrap.test.tsx
+++ packages/core/test/wrap.test.tsx
@@ -1,6 +1,11 @@
+import { logger, setCurrentClient } from '@sentry/core';
+import { render } from '@testing-library/react-native';
 import * as React from 'react';
+import { Text } from 'react-native';
 
+import * as AppRegistry from '../src/js/integrations/appRegistry';
 import { wrap } from '../src/js/sdk';
+import { getDefaultTestClientOptions, TestClient } from './mocks/client';
 
 describe('Sentry.wrap', () => {
   it('should not enforce any keys on the wrapped component', () => {
@@ -8,5 +13,23 @@ describe('Sentry.wrap', () => {
     const ActualWrapped = wrap(Mock);
 
     expect(typeof ActualWrapped.defaultProps).toBe(typeof Mock.defaultProps);
+  });
+
+  it('should wrap the component and init with a warning when getAppRegistryIntegration returns undefined', () => {
+    jest.spyOn(AppRegistry, 'getAppRegistryIntegration').mockReturnValueOnce(undefined);
+    logger.warn = jest.fn();
+
+    const Mock: React.FC = () => <Text>Test</Text>;
+    const client = new TestClient(
+      getDefaultTestClientOptions(),
+    );
+    setCurrentClient(client);
+
+    client.init();
+    const ActualWrapped = wrap(Mock);
+
+    const { getByText } = render(<ActualWrapped />);
+    expect(logger.warn).toHaveBeenCalledWith('AppRegistryIntegration.onRunApplication not found or invalid.');
+    expect(getByText('Test')).toBeTruthy();
   });
 });
