14ad53a4966f612d0f8dad31a87d3e6673d2b2c0
lucas <lucas-zimerman1@hotmail.com>
2025-05-22 10:01:20 -0300

    enable android log

--- packages/core/android/src/main/java/io/sentry/react/RNSentryModuleImpl.java
+++ packages/core/android/src/main/java/io/sentry/react/RNSentryModuleImpl.java
@@ -278,6 +278,12 @@ public class RNSentryModuleImpl {
     if (rnOptions.hasKey("enableNdk")) {
       options.setEnableNdk(rnOptions.getBoolean("enableNdk"));
     }
+    if (rnOptions.hasKey("_experiments")) {
+        var experiments = rnOptions.getMap("_experiments");
+        if (experiments.hasKey(("enableLogs"))) {
+          options.getLogs().setEnabled(experiments.getBoolean("enableLogs"));
+        }
+    }
     if (rnOptions.hasKey("spotlight")) {
       if (rnOptions.getType("spotlight") == ReadableType.Boolean) {
         options.setEnableSpotlight(rnOptions.getBoolean("spotlight"));
