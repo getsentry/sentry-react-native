6e5e24b8db8c1d142239609a43dadf72659fa04c
lucas <lucas-zimerman1@hotmail.com>
2025-06-16 16:42:05 -0300

    removed comment

--- samples/expo/app/(tabs)/index.tsx
+++ samples/expo/app/(tabs)/index.tsx
@@ -17,7 +17,6 @@ export default function TabOneScreen() {
   const { currentlyRunning } = useUpdates();
   return (
     <View style={styles.container}>
-      {/* <Image source={require('../..//hi.gif')} style={{ width: 100, height: 100 }} /> */}
       <Sentry.TimeToInitialDisplay record />
       <Text>Welcome to Sentry Expo Sample App!</Text>
       <Text>Update ID: {currentlyRunning.updateId}</Text>
