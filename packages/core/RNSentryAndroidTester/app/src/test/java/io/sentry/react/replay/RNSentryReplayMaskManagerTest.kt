e2921ea9bfc84f76d2626080edb98900a9c8a720
Antonis Lilis <antonis.lilis@gmail.com>
2025-07-01 14:50:18 +0300

    test: Adds unit tests for SR mask/unmask managers

---
+++ packages/core/RNSentryAndroidTester/app/src/test/java/io/sentry/react/replay/RNSentryReplayMaskManagerTest.kt
@@ -0,0 +1,38 @@
+package io.sentry.react.replay
+
+import com.facebook.react.module.annotations.ReactModule
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import org.mockito.MockitoAnnotations
+
+@RunWith(JUnit4::class)
+class RNSentryReplayMaskManagerTest {
+    private val expectedName = RNSentryReplayMaskManagerImpl.REACT_CLASS
+
+    private lateinit var manager: RNSentryReplayMaskManager
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.openMocks(this)
+        manager = RNSentryReplayMaskManager()
+    }
+
+    @Test
+    fun `getName returns correct react class name`() {
+        assertEquals(expectedName, manager.getName())
+    }
+
+    @Test
+    fun `module annotation name matches getName result`() {
+        val annotation = manager.javaClass.getAnnotation(ReactModule::class.java)
+        assertNotNull("ReactModule annotation should be present", annotation)
+        assertEquals(
+            "Annotation name should match getName() result",
+            expectedName,
+            annotation?.name,
+        )
+    }
+}
---
+++ packages/core/RNSentryAndroidTester/app/src/test/java/io/sentry/react/replay/RNSentryReplayUnmaskManagerTest.kt
@@ -0,0 +1,38 @@
+package io.sentry.react.replay
+
+import com.facebook.react.module.annotations.ReactModule
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import org.mockito.MockitoAnnotations
+
+@RunWith(JUnit4::class)
+class RNSentryReplayUnmaskManagerTest {
+    private val expectedName = RNSentryReplayUnmaskManagerImpl.REACT_CLASS
+
+    private lateinit var manager: RNSentryReplayUnmaskManager
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.openMocks(this)
+        manager = RNSentryReplayUnmaskManager()
+    }
+
+    @Test
+    fun `getName returns correct react class name`() {
+        assertEquals(expectedName, manager.getName())
+    }
+
+    @Test
+    fun `module annotation name matches getName result`() {
+        val annotation = manager.javaClass.getAnnotation(ReactModule::class.java)
+        assertNotNull("ReactModule annotation should be present", annotation)
+        assertEquals(
+            "Annotation name should match getName() result",
+            expectedName,
+            annotation?.name,
+        )
+    }
+}
