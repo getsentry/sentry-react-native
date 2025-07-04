413ece71e5bdb91769d148d1609655d05b62bec3
lucas <lucas-zimerman1@hotmail.com>
2025-07-04 18:12:26 -0300

    split expo changes UI

--- samples/expo/app/(tabs)/index.tsx
+++ samples/expo/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { Button, ScrollView, StyleSheet } from 'react-native';
+import { Button, StyleSheet } from 'react-native';
 import * as Sentry from '@sentry/react-native';
 import { reloadAppAsync, isRunningInExpoGo } from 'expo';
 import * as DevClient from 'expo-dev-client';
@@ -19,181 +19,101 @@ export default function TabOneScreen() {
       <Text>Update ID: {currentlyRunning.updateId}</Text>
       <Text>Channel: {currentlyRunning.channel}</Text>
       <Text>Runtime Version: {currentlyRunning.runtimeVersion}</Text>
-      <View style={styles.container}>
-        <ScrollView
-        >
-          <View style={styles.buttonWrapper}>
-            <Button
-              title="Open DevMenu"
-              onPress={() => {
-                DevClient.openMenu();
-              }}
-              disabled={isWeb()}
-            />
-          </View>
-          <View style={styles.buttonWrapper}>
-            <Button
-              title="Capture message"
-              onPress={() => {
-                Sentry.captureMessage('Captured message');
-              }}
-            />
-          </View>
-          <View style={styles.buttonWrapper}>
-            <Button
-              title="Capture exception"
-              onPress={() => {
-                Sentry.captureException(new Error('Captured exception'));
-              }}
-            />
-          </View>
-          <View style={styles.buttonWrapper}>
-            <Button
-              title="Capture exception with cause"
-              onPress={() => {
-                const error = new Error('Captured exception');
-                error.cause = new Error('Cause of captured exception');
-                Sentry.captureException(error);
-              }}
-            />
-          </View>
-          <View style={styles.buttonWrapper}>
-            <Button
-              title="Uncaught Thrown Error"
-              onPress={() => {
-                throw new Error('Uncaught Thrown Error');
-              }}
-            />
-          </View>
-          <View style={styles.buttonWrapper}>
-            <Button
-              title="Unhandled Promise Rejection"
-              onPress={() => {
-                // TODO: No working in Expo Go App
-                Promise.reject(new Error('Unhandled Promise Rejection'));
-              }}
-            />
-          </View>
-          <View style={styles.buttonWrapper}>
-            <Button
-              title="Native Crash"
-              onPress={() => {
-                if (isRunningInExpoGo()) {
-                  console.warn('Not supported in Expo Go. Build the application to test this feature.');
-                  return;
-                }
-                Sentry.nativeCrash();
-              }}
-            />
-          </View>
-          <View style={styles.buttonWrapper}>
-            <Button
-              title="Show feedback form"
-              onPress={() => {
-                Sentry.showFeedbackWidget();
-              }}
-            />
-          </View>
-          <View style={styles.buttonWrapper}>
-            <Button
-              title="Show feedback button"
-              onPress={() => {
-                Sentry.showFeedbackButton();
-              }}
-            />
-          </View>
-          <View style={styles.buttonWrapper}>
-            <Button
-              title="Set Scope Properties"
-              onPress={() => {
-                setScopeProperties();
-              }}
-            />
-          </View>
-          <View style={styles.buttonWrapper}>
-            <Button
-              title="console.warn()"
-              onPress={() => {
-                console.warn('This is a warning.');
-              }}
-            />
-          </View>
-          <View style={styles.buttonWrapper}>
-            <Button
-              title="Flush"
-              onPress={async () => {
-                await Sentry.flush();
-                console.log('Sentry.flush() completed.');
-              }}
-            />
-          </View>
-          <View style={styles.buttonWrapper}>
-            <Button
-              title="Close"
-              onPress={async () => {
-                await Sentry.close();
-                console.log('Sentry.close() completed.');
-              }}
-            />
-          </View>
-          <View style={styles.buttonWrapper}>
-            <Button title="Reload" onPress={() => reloadAppAsync()} />
-          </View>
-          <View style={styles.buttonWrapper}>
-            <Button
-              title="Open WebBrowser"
-              onPress={() => {
-                WebBrowser.openBrowserAsync('https://sentry.io');
-              }}
-            />
-          </View>
-          <View style={styles.buttonWrapper}>
-            <Button
-              title="Set different types of tags globally"
-              onPress={async () => {
-                Sentry.setTags({
-                  number: 123,
-                  boolean: true,
-                  null: null,
-                  undefined: undefined,
-                  symbol: Symbol('symbol'),
-                  string: 'string',
-                  bigint: BigInt(123),
-                });
-                Sentry.captureMessage('Message with different types of tags globally');
-                Sentry.setTags({
-                  number: undefined,
-                  boolean: undefined,
-                  null: undefined,
-                  symbol: undefined,
-                  string: undefined,
-                  bigint: undefined,
-                });
-              }}
-            />
-          </View>
-          <View style={styles.buttonWrapper}>
-            <Button
-              title="Set different types of tags in scope"
-              onPress={async () => {
-                const evt = {
-                  message: 'Message with different types of tags isolated',
-                  tags: {
-                    number: 123,
-                    boolean: true,
-                    null: null,
-                    undefined: undefined,
-                    symbol: Symbol('symbol'),
-                    string: 'abc',
-                    bigint: BigInt(123),
-                  },
-                };
-                Sentry.captureEvent(evt);
-              }}
-            />
-          </View>
-        </ScrollView>
-      </View>
+      <Button
+        title="Open DevMenu"
+        onPress={() => {
+          DevClient.openMenu();
+        }}
+        disabled={isWeb()}
+      />
+      <Button
+        title="Capture message"
+        onPress={() => {
+          Sentry.captureMessage('Captured message');
+        }}
+      />
+      <Button
+        title="Capture exception"
+        onPress={() => {
+          Sentry.captureException(new Error('Captured exception'));
+        }}
+      />
+      <Button
+        title="Capture exception with cause"
+        onPress={() => {
+          const error = new Error('Captured exception');
+          error.cause = new Error('Cause of captured exception');
+          Sentry.captureException(error);
+        }}
+      />
+      <Button
+        title="Uncaught Thrown Error"
+        onPress={() => {
+          throw new Error('Uncaught Thrown Error');
+        }}
+      />
+      <Button
+        title="Unhandled Promise Rejection"
+        onPress={() => {
+          // TODO: No working in Expo Go App
+          Promise.reject(new Error('Unhandled Promise Rejection'));
+        }}
+      />
+      <Button
+        title="Native Crash"
+        onPress={() => {
+          if (isRunningInExpoGo()) {
+            console.warn('Not supported in Expo Go. Build the application to test this feature.');
+            return;
+          }
+          Sentry.nativeCrash();
+        }}
+      />
+      <Button
+        title="Show feedback form"
+        onPress={() => {
+          Sentry.showFeedbackWidget();
+        }}
+      />
+      <Button
+        title="Show feedback button"
+        onPress={() => {
+          Sentry.showFeedbackButton();
+        }}
+      />
+      <Button
+        title="Set Scope Properties"
+        onPress={() => {
+          setScopeProperties();
+        }}
+      />
+      <Button
+        title="console.warn()"
+        onPress={() => {
+          console.warn('This is a warning.');
+        }}
+      />
+      <Button
+        title="Flush"
+        onPress={async () => {
+          await Sentry.flush();
+          console.log('Sentry.flush() completed.');
+        }}
+      />
+      <Button
+        title="Close"
+        onPress={async () => {
+          await Sentry.close();
+          console.log('Sentry.close() completed.');
+        }}
+      />
+      <Button title="Reload" onPress={() => reloadAppAsync()} />
+      <Button
+        title="Open WebBrowser"
+        onPress={() => {
+          WebBrowser.openBrowserAsync('https://sentry.io');
+        }}
+      />
     </View>
   );
 }
@@ -212,9 +132,5 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
-  },
-  buttonWrapper: {
-    marginVertical: 6,
-    marginHorizontal: 12,
   },
 });
