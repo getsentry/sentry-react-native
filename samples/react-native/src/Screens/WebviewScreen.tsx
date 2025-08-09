6c8e4e13af21a6f67b102d8f015555c670d94374
Alexander Pantiukhov <alex@apantiukhov.com>
2025-08-09 10:17:50 +0200

    feat(sampleApps): update sample apps to React Native 0.80 / update other deps

--- samples/react-native/src/Screens/WebviewScreen.tsx
+++ samples/react-native/src/Screens/WebviewScreen.tsx
@@ -1,20 +1,13 @@
 import * as React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Text } from 'react-native-gesture-handler';
-// import { WebView } from 'react-native-webview';
+import { StyleSheet } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 const WebviewScreen = () => {
   return (
-    // <WebView
-    //   source={{ uri: 'https://sentry.io' }}
-    //   style={styles.webview}
-    // />
-    <View style={styles.webview}>
-      <Text>
-        WebView is disabled due to incompatibilities with the React Native
-        version 0.79.2
-      </Text>
-    </View>
+    <WebView
+      source={{ uri: 'https://sentry.io' }}
+      style={styles.webview}
+    />
   );
 };
 
