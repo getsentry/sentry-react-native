f5cac1bd444605c4693fd4e6bd064c547bbb31bb
Alexander Pantiukhov <alex@apantiukhov.com>
2025-08-09 10:17:50 +0200

    feat(sampleApps): update sample apps to React Native 0.80 / update other deps

--- samples/react-native/src/dsn.ts
+++ samples/react-native/src/dsn.ts
@@ -4,5 +4,5 @@ export const SENTRY_INTERNAL_DSN =
   'https://1df17bd4e543fdb31351dee1768bb679@o447951.ingest.sentry.io/5428561';
 
 export const getCurrentDsn = () => {
-  return Sentry.getCurrentHub().getClient()?.getOptions().dsn;
+  return Sentry.getClient()?.getOptions().dsn;
 };
