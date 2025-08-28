1e2c7ce23640c0f50b509b6548c7b4bea6824809
Antonis Lilis <antonis.lilis@gmail.com>
2025-08-28 19:53:08 +0200

    Apply suggestions from code review
    Co-authored-by: LucasZF <lucas-zimerman1@hotmail.com>

--- packages/core/src/js/feedback/FeedbackWidget.tsx
+++ packages/core/src/js/feedback/FeedbackWidget.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable max-lines */
 import type { SendFeedbackParams, User } from '@sentry/core';
-import { captureFeedback, debug,getCurrentScope, getGlobalScope, getIsolationScope, lastEventId } from '@sentry/core';
+import { captureFeedback, debug, getCurrentScope, getGlobalScope, getIsolationScope, lastEventId } from '@sentry/core';
 import * as React from 'react';
 import type { KeyboardTypeOptions ,
   NativeEventSubscription} from 'react-native';
