a7de1bc6e0eb60f9849ad5cf0521d76832d9e294
Noah Martin <noahm444@gmail.com>
2025-09-03 02:13:38 -1000

    Fix: Fix compile error in v7 (#5134)
    * Fix: Fix compile error in v7
    
    * Update packages/core/ios/RNSentry.mm
    
    Co-authored-by: Antonis Lilis <antonis.lilis@gmail.com>
    
    * Remove extra space
    
    ---------
    
    Co-authored-by: LucasZF <lucas-zimerman1@hotmail.com>
    Co-authored-by: Antonis Lilis <antonis.lilis@gmail.com>

--- packages/core/ios/RNSentry.mm
+++ packages/core/ios/RNSentry.mm
@@ -467,7 +467,11 @@ RCT_EXPORT_METHOD(fetchNativeLogAttributes
             contexts[@"os"] = os;
         }
 
+#if CROSS_PLATFORM_TEST
+        NSString *releaseName = SentrySDKInternal.options.releaseName;
+#else
         NSString *releaseName = [SentrySDK options].releaseName;
+#endif
         if (releaseName) {
             contexts[@"release"] = releaseName;
         }
