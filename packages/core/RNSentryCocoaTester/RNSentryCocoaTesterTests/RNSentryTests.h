a531de33bced0e2326fed21713101adbf24401e7
Antonis Lilis <antonis.lilis@sentry.io>
2025-09-30 11:56:27 +0200

    Cherry pick cocoa version bump fixes from https://github.com/getsentry/sentry-react-native/pull/5036

--- packages/core/RNSentryCocoaTester/RNSentryCocoaTesterTests/RNSentryTests.h
+++ packages/core/RNSentryCocoaTester/RNSentryCocoaTesterTests/RNSentryTests.h
@@ -3,12 +3,8 @@
 
 @class SentryOptions;
 
-#if CROSS_PLATFORM_TEST
 @interface
 SentrySDKInternal (PrivateTests)
-#else
-@interface
-SentrySDK (PrivateTests)
-#endif
+
 + (nullable SentryOptions *)options;
 @end
