28301bd1dcc5cc095967b6610968e5675363de94
Alexander Pantiukhov <alex@apantiukhov.com>
2025-10-23 09:34:00 +0200

    Tests for Supabase integration

--- packages/core/test/integrations/supabase.test.ts
+++ packages/core/test/integrations/supabase.test.ts
@@ -1,13 +1,29 @@
-import * as environment from '../../src/js/utils/environment';
+import { supabaseIntegration as browserSupabaseIntegration } from '@sentry/browser';
+import { supabaseIntegration } from '../../src/js';
 
 jest.mock('@sentry/browser', () => ({
   supabaseIntegration: jest.fn(),
 }));
+
+function createMockClient() {
+  return {
+    init: jest.fn(),
+  };
+}
 
 describe('supabase', () => {
   afterEach(() => {
     jest.resetAllMocks();
   });
 
+  // this test is sufficient to test the integration because everything else
+  // is covered by the integration tests in the @sentry/browser package
+  it('passes React Native options to browserSupabaseIntegration', () => {
+    const mockClient = createMockClient();
+    supabaseIntegration({supabaseClient: mockClient});
 
+    expect(browserSupabaseIntegration).toHaveBeenCalledWith({
+      supabaseClient: mockClient,
+    });
+  });
 });
