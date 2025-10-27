0327771ba20101489d5fbe3ede3b7d5b6fea770c
Noah Martin <noahm444@gmail.com>
2025-10-27 10:37:41 -0400

    Remove use of integrations in tests

--- packages/core/RNSentryCocoaTester/RNSentryCocoaTesterTests/RNSentryTests.m
+++ packages/core/RNSentryCocoaTester/RNSentryCocoaTesterTests/RNSentryTests.m
@@ -65,8 +65,7 @@ XCTAssertEqual(actualOptions.tracesSampler, nil, @"Traces sampler should not be 
                                                                    error:&error];
     XCTAssertNotNil(actualOptions, @"Did not create sentry options");
     XCTAssertNil(error, @"Should not pass no error");
-    XCTAssertEqual([actualOptions.integrations containsObject:@"SentryCrashIntegration"], true,
-        @"Did not set native crash handling");
+    XCTAssertTrue(actualOptions.enableCrashHandler, @"Did not set native crash handling");
 }
 
 - (void)testCreateOptionsWithDictionaryAutoPerformanceTracingDefault
@@ -98,8 +97,7 @@ XCTAssertEqual(actualOptions.tracesSampler, nil, @"Traces sampler should not be 
                                                                    error:&error];
     XCTAssertNotNil(actualOptions, @"Did not create sentry options");
     XCTAssertNil(error, @"Should not pass no error");
-    XCTAssertEqual([actualOptions.integrations containsObject:@"SentryCrashIntegration"], true,
-        @"Did not set native crash handling");
+    XCTAssertTrue(actualOptions.enableCrashHandler, @"Did not set native crash handling");
 }
 
 - (void)testCreateOptionsWithDictionaryAutoPerformanceTracingEnabled
