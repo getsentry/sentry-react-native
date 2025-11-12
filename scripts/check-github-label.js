7d36455caed89fbdaa08b62084be62880225aec7
lucas <lucas-zimerman1@hotmail.com>
2025-11-12 10:33:11 +0000

    just error for all

--- scripts/check-github-label.js
+++ scripts/check-github-label.js
@@ -1,11 +1,7 @@
 module.exports = async function ({ fail, warn, __, ___, danger }) {
-  console.log(JSON.stringify(danger));
-  const isReadyForReview = danger.github.event.pull_request?.draft === false;
   const isReeadyForMerge = danger.github.issue.labels.some(label => label.name === 'ready-for-merge');
-  if (isReadyForReview && !isReeadyForMerge) {
+  if (!isReeadyForMerge) {
+    // Ideally we could check for `isDraft` but this information is not easily available.
     fail('Pull request is not ready for merge, please add the "ready-for-merge" label to the pull request');
-  }
-  else if (!isReadyForReview) {
-    warn('Pull request is a draft, please add the "ready-for-merge" label to the pull request when it is ready for review');
   }
 };
