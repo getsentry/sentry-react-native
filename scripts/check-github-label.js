46ce60122616adb6dbb2a301ccdfd3777f3ab3e4
lucas <lucas-zimerman1@hotmail.com>
2025-11-12 11:07:42 +0000

    fixed label name

--- scripts/check-github-label.js
+++ scripts/check-github-label.js
@@ -1,8 +1,7 @@
 module.exports = async function ({ fail, warn, __, ___, danger }) {
-  console.log(JSON.stringify(danger.github.issue.labels, null, 2));
-  const isReeadyForMerge = danger.github.issue.labels.some(label => label.name === 'ready-for-merge');
+  const isReeadyForMerge = danger.github.issue.labels.some(label => label.name === 'ready-to-merge');
   if (!isReeadyForMerge) {
     // Ideally we could check for `isDraft` but this information is not easily available.
-    fail('Pull request is not ready for merge, please add the "ready-for-merge" label to the pull request');
+    fail('Pull request is not ready for merge, please add the "ready-to-merge" label to the pull request');
   }
 };
