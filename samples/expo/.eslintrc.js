f23c34cedfaf0f5932c00a76a930038b882cfc6b
Alexander Pantiukhov <alex@apantiukhov.com>
2025-12-22 10:55:11 +0100

    Attempt to fix on CI

--- samples/expo/.eslintrc.js
+++ samples/expo/.eslintrc.js
@@ -3,6 +3,14 @@ module.exports = {
   extends: ['expo', '@react-native'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project: ['./tsconfig.json', '../../packages/core/tsconfig.json'],
+        noWarnOnMultipleProjects: true,
+      },
+    },
+  },
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
