0544c2a0c67dc0e1fea1e2cfee38b2979632bab0
Antonis Lilis <antonis.lilis@sentry.io>
2026-01-21 16:37:37 +0100

    Fix lint issue

--- packages/core/RNSentryCocoaTester/RNSentryCocoaTesterTests/RNSentryCocoaTesterTests-Bridging-Header.h
+++ packages/core/RNSentryCocoaTester/RNSentryCocoaTesterTests/RNSentryCocoaTesterTests-Bridging-Header.h
@@ -2,9 +2,9 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+#import <React/RCTViewManager.h>
 #import <Sentry/Sentry.h>
 #import <Sentry/SentryFramesTracker.h>
-#import <React/RCTViewManager.h>
 
 #import "../RNSentrySDK+Test.h"
 #import "RNSentryBreadcrumb.h"
