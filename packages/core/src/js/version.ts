89b49b31a98f44a2d569d589fd826f396fcaf105
alwx <911127+alwx@users.noreply.github.com>
2026-01-29 12:51:43 +0000

    release: 7.11.0

--- packages/core/src/js/version.ts
+++ packages/core/src/js/version.ts
@@ -1,3 +1,3 @@
 export const SDK_PACKAGE_NAME = 'npm:@sentry/react-native';
 export const SDK_NAME = 'sentry.javascript.react-native';
-export const SDK_VERSION = '7.10.0';
+export const SDK_VERSION = '7.11.0';
