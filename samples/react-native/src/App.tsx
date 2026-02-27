6d13f6add8228c3d0f5751dab4de00c229958571
Antonis Lilis <antonis.lilis@sentry.io>
2026-02-27 16:03:37 +0100

    Revert "test(sample): add FeedbackWidgetProvider to React Native sample app"
    This reverts commit 6054e19eee49ac4b2669a51e9c98845e1b71fe71.

--- samples/react-native/src/App.tsx
+++ samples/react-native/src/App.tsx
@@ -10,7 +10,6 @@ import {
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createStackNavigator } from '@react-navigation/stack';
 import * as Sentry from '@sentry/react-native';
-import { FeedbackWidgetProvider } from '@sentry/react-native';
 import { isTurboModuleEnabled } from '@sentry/react-native/dist/js/utils/environment';
 import { LogBox, Platform } from 'react-native';
 import * as ImagePicker from 'react-native-image-picker';
@@ -260,10 +259,10 @@ function RootNavigationContainer() {
 
 function App() {
   return (
-    <FeedbackWidgetProvider>
+    <>
       <RootNavigationContainer />
       <RunningIndicator />
-    </FeedbackWidgetProvider>
+    </>
   );
 }
 
