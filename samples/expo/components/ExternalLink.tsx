06c92d00b3586f26849fb04f18662b4ba7c96bf2
Antonis Lilis <antonis.lilis@sentry.io>
2026-03-30 16:55:12 +0200

    chore(lint): Commit local lint fixes

--- samples/expo/components/ExternalLink.tsx
+++ samples/expo/components/ExternalLink.tsx
@@ -3,9 +3,7 @@ import * as WebBrowser from 'expo-web-browser';
 import React from 'react';
 import { Platform } from 'react-native';
 
-export function ExternalLink(
-  props: Omit<React.ComponentProps<typeof Link>, 'href'> & { href: string },
-) {
+export function ExternalLink(props: Omit<React.ComponentProps<typeof Link>, 'href'> & { href: string }) {
   return (
     <Link
       target="_blank"
