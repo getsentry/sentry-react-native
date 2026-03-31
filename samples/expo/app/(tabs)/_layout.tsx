81d2659bf30499fd2a095393837c6f8bd101aa91
Antonis Lilis <antonis.lilis@sentry.io>
2026-03-31 11:16:01 +0200

    chore(lint): Commit local lint fixes (#5928)

--- samples/expo/app/(tabs)/_layout.tsx
+++ samples/expo/app/(tabs)/_layout.tsx
@@ -1,17 +1,14 @@
-import React from 'react';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Link, Tabs } from 'expo-router';
+import React from 'react';
 import { Pressable } from 'react-native';
 
-import Colors from '@/constants/Colors';
-import { useColorScheme } from '@/components/useColorScheme';
 import { useClientOnlyValue } from '@/components/useClientOnlyValue';
+import { useColorScheme } from '@/components/useColorScheme';
+import Colors from '@/constants/Colors';
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
+function TabBarIcon(props: { name: React.ComponentProps<typeof FontAwesome>['name']; color: string }) {
   return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
 }
 
@@ -39,10 +36,7 @@ function InfoButton({ colorScheme }: { colorScheme: 'light' | 'dark' | null }) {
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
-  const renderInfoButton = React.useCallback(
-    () => <InfoButton colorScheme={colorScheme} />,
-    [colorScheme],
-  );
+  const renderInfoButton = React.useCallback(() => <InfoButton colorScheme={colorScheme} />, [colorScheme]);
 
   return (
     <Tabs
@@ -51,7 +45,8 @@ export default function TabLayout() {
         // Disable the static render of the header on web
         // to prevent a hydration error in React Navigation v6.
         headerShown: useClientOnlyValue(false, true),
-      }}>
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
