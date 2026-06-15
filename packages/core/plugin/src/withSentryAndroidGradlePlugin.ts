3b5111af8022772a2ae7c48f40fc08078ae392ce
GitHub <noreply@github.com>
2026-06-15 07:45:06 +0000

    chore: update scripts/update-sentry-android-gradle-plugin.sh to 6.11.0

--- packages/core/plugin/src/withSentryAndroidGradlePlugin.ts
+++ packages/core/plugin/src/withSentryAndroidGradlePlugin.ts
@@ -15,7 +15,7 @@ export interface SentryAndroidGradlePluginOptions {
   includeSourceContext?: boolean;
 }
 
-export const sentryAndroidGradlePluginVersion = '6.10.0';
+export const sentryAndroidGradlePluginVersion = '6.11.0';
 
 /**
  * Adds the Sentry Android Gradle Plugin to the project.
