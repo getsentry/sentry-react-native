79001c5e7d64f2950ce7578743a240062837b646
Antonis Lilis <antonis.lilis@sentry.io>
2026-09-04 15:16:42 +0200

    Update test text to look for

--- samples/react-native-visionos/visionos/VisionSampleTests/VisionSampleTests.m
+++ samples/react-native-visionos/visionos/VisionSampleTests/VisionSampleTests.m
@@ -5,7 +5,7 @@
 #import <React/RCTRootView.h>
 
 #define TIMEOUT_SECONDS 600
-#define TEXT_TO_LOOK_FOR @"Welcome to React"
+#define TEXT_TO_LOOK_FOR @"Sentry React Native visionOS"
 
 @interface VisionSampleTests : XCTestCase
 
